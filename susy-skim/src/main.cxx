df603e55ba0617da5c7a066aa2118a6dbcd79a59
Dan Guest <dguest@cern.ch>
2012-07-04 18:48:25 +0200

    added some functions from SUSYTools

--- susy-skim/src/main.cxx
+++ susy-skim/src/main.cxx
@@ -9,6 +9,9 @@ int main (int narg, char* argv[])
 {
 
   SUSYObjDef def; 
+  RunInfo info; 
+  info.is_data = false; 
+  info.run_number = 180614; 
 
   susy buffer; 
   TTree* tree = buffer.fChain; 
@@ -17,7 +20,21 @@ int main (int narg, char* argv[])
 
   for (int evt_n = 0; evt_n < 10; evt_n++) { 
     tree->GetEntry(evt_n); 
-    
+
+    // event preselection 
+    bool lar_error = buffer.larError; 
+
+    const int n_jets = buffer.jet_AntiKt4TopoNewEM_n; 
+
+    for (int jet_n = 0; jet_n < n_jets; jet_n++){ 
+      // jet preselection 
+      bool lar_hole_veto = check_lar_hole_veto(jet_n, buffer, def, info); 
+
+      bool is_jet = check_if_jet(jet_n, buffer, def, info); 
+
+    }
+
+
     std::cout << buffer.el_n << " electrons in event " << evt_n
 	      << std::endl; 
     std::cout << "in crack? " << (def.IsInCrack(0.4) ? "yes":"no" )
@@ -26,4 +43,58 @@ int main (int narg, char* argv[])
 
   return 0;
 }
+
+bool check_lar_hole_veto(int jet_n, const susy& buffer, SUSYObjDef& def, 
+		       const RunInfo& info ) { 
+  int n = jet_n; 
+  bool veto = def.IsLArHoleVeto
+    (buffer.jet_AntiKt4TopoNewEM_pt->at(n), 
+     buffer.jet_AntiKt4TopoNewEM_eta->at(n), 
+     buffer.jet_AntiKt4TopoNewEM_phi->at(n),
+     buffer.jet_AntiKt4TopoNewEM_BCH_CORR_JET->at(n), 
+     buffer.jet_AntiKt4TopoNewEM_BCH_CORR_CELL->at(n), 
+     info.is_data, 
+     40000., // pt cut, not sure why needed...
+     info.run_number);
+  if (veto) return true; 
+  return false; 
+}
+
+bool check_if_jet(int iJet, 
+		  const susy& buffer, 
+		  SUSYObjDef& def, 
+		  const RunInfo& info){ 
+
+  return IsJet
+    (iJet, 
+     buffer.jet_AntiKt4TopoNewEM_pt                 ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_eta                ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_phi                ->at(iJet),
+     buffer.jet_AntiKt4TopoNewEM_E                  ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_emscale_eta        ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_emfrac             ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_hecf               ->at(iJet),
+     buffer.jet_AntiKt4TopoNewEM_LArQuality         ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_HECQuality         ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_AverageLArQF       ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_Timing             ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_sumPtTrk           ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_fracSamplingMax    ->at(iJet),
+     buffer.jet_AntiKt4TopoNewEM_NegativeE          ->at(iJet), 
+     buffer.jet_AntiKt4TopoNewEM_flavor_truth_label ->at(iJet), 
+     buffer.jet_emscale_E                           ->at(iJet),
+     buffer.jet_emscale_eta                         ->at(iJet), 
+     buffer.jet_EtaOrigin                           ->at(iJet), 
+     buffer.jet_PhiOrigin                           ->at(iJet), 
+     buffer.jet_MOrigin                             ->at(iJet), 
+     buffer.averageIntPerXing,
+     buffer.vx_nTracks,             
+     info.is_data, 
+     20000., 			// pt cut
+     2.8,			// eta cut
+     JetID::LooseBad,
+     SystErr::NONE);
+
+}
+
 
--- susy-skim/src/main.hh
+++ susy-skim/src/main.hh
@@ -1,5 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+class susy; 
+class SUSYObjDef; 
+
+struct RunInfo { 
+  bool is_data; 
+  int run_number; 
+}; 
+bool check_lar_hole_veto(int jet_n, 
+			 const susy& buffer, 
+			 SUSYObjDef& def, 
+			 const RunInfo&); 
+
+bool check_if_jet(int jet_n, 
+		  const susy& buffer, 
+		  SUSYObjDef& def, 
+		  const RunInfo&); 
 
 #endif //MAIN_H
