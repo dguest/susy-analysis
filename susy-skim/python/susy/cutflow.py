07dabe79cd7c92d5cc218e4766059a42c8d250be
Dan Guest <dguest@cern.ch>
2012-09-12 19:15:25 +0200

    add caching routine for cutflow results. the numbers I'm getting still don't make any sense..

--- susy-skim/python/susy/cutflow.py
+++ susy-skim/python/susy/cutflow.py
@@ -1,5 +1,7 @@
 import _cutflow 
 import warnings
+import cPickle
+import os
 
 run_number = 180614
 
@@ -15,8 +17,12 @@ def cutflow(input_file, run_number, flags):
 
 
 class NormedCutflow(object): 
-    def __init__(self, norm_file): 
+    """
+    Uses a lookup file to normalize cutflows. 
+    """
+    def __init__(self, norm_file, raw_counts_cache = 'raw_counts.pkl'): 
         self._norm_file_name = norm_file
+        self._cache_name = raw_counts_cache
         with open(norm_file) as txt: 
             self._norm_dict = {n: (x,e) for n,x,e in self._tup_itr(txt)}
 
@@ -31,14 +37,46 @@ class NormedCutflow(object):
             evts = int(spl[4])
             yield short_name, xsec, evts
 
-    def get_normed_counts(self, file_name): 
+    def get_normed_counts(self, ds_key): 
         
-        if not file_name in self._norm_dict: 
+        if not ds_key in self._norm_dict: 
             raise LookupError('no {} found in {}'.format(
                     file_name, self._norm_file_name))
 
-        cut_counts = cutflow(input_file, run_number, 'vs')
+        cached_info = {}
+        if os.path.isfile(self._cache_name): 
+            with open(self._cache_name,'rb') as pkl: 
+                cached_info = cPickle.load(pkl)
+
+        if not ds_key in cached_info: 
+            input_file = ds_key + '.root'
+            cut_counts = cutflow(input_file, run_number, 'vs')
+            
+            cached_info[ds_key] = cut_counts
+
+            with open(self._cache_name,'wb') as pkl: 
+                cPickle.dump(cached_info, pkl)
+
+        cut_counts = cached_info[ds_key]
         
+        # we assume the maximum cut is the number of events
+        n_events = max(count for name, count in cut_counts)
+        cross_section, xsec_events = self._norm_dict[ds_key]
+        frac_in_file = float(n_events) / float(xsec_events)
+        if n_events != xsec_events: 
+            warnings.warn(
+                "number of events in {} ({}) "
+                "doesn't match expected {} ({:.1%} diff)".format(
+                    ds_key, n_events, xsec_events, frac_in_file - 1), 
+                stacklevel=2)
+        
+        robust_xsec =  cross_section / frac_in_file
+
+        normed_counts = {n: c * robust_xsec for n,c in cut_counts}
+
+        return normed_counts
+        
+                
                               
 
 def print_raw_cutflow(input_file): 
--- susy-skim/run_cutflow.py
+++ susy-skim/run_cutflow.py
@@ -4,8 +4,23 @@ from susy import cutflow
 import sys
 
 
-# n = cutflow.NormedCutflow('SampleListStop.txt')
+normed_cutflow = cutflow.NormedCutflow('SampleListStop.txt')
 
-cutflow.print_raw_cutflow(sys.argv[1])
+sig_counts = {}
+for sig in ['Stop-180-100_1excl','Stop-180-100_2incl']: 
+    sig_counts[sig] = normed_cutflow.get_normed_counts(sig)
+    
+cuts_total = {}
+for cf in sig_counts.values(): 
+    if not cuts_total: 
+        cuts_total = cf
+    else: 
+        for cut in cuts_total: 
+            cuts_total[cut] += cf[cut]
+
+sorted_cuts = sorted(cuts_total.items(), key=lambda cut: cut[1], reverse=True)
+           
+for name, count in sorted_cuts: 
+    print '{:>20}: {: >12.2f}'.format(name,count)
 
 
