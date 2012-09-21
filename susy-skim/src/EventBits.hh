b96605faf7357197e17e42b5edbef5a53d84c858
Dan Guest <dguest@cern.ch>
2012-09-21 12:58:42 +0200

    comment

--- susy-skim/src/EventBits.hh
+++ susy-skim/src/EventBits.hh
@@ -1,6 +1,9 @@
 #ifndef EVENT_BITS_HH
 #define EVENT_BITS_HH
 
+// NOTE: This is (to be) parsed by a python function to extract bit 
+//       definitions. Please restrict the below code to bit defs. 
+
 namespace evt { 
   const unsigned pass_leading_pt  = 1u << 0; 
   const unsigned pass_met         = 1u << 1; 
