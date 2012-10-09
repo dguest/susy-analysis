a6a45170bd962da993762c515531cfaef9378469
Dan Guest <dguest@cern.ch>
2012-10-09 12:30:19 +0200

    format get_event_bits as a python dict

--- susy-skim/get_event_bits.sh
+++ susy-skim/get_event_bits.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -n -r 's/^ *const unsigned ([^ ]*) *=([^;]*);/\1 = \2/gp'\
+sed -n -r 's/^ *const unsigned ([^ ]*) *=([^;]*);/\(\"\1\", \2\),/gp'\
  include/EventBits.hh | sed 's/1u/1/g' 
