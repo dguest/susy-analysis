ef7e2132897307768967a1faeaa06970f3643759
Dan Guest <dguest@cern.ch>
2012-11-05 15:16:55 +0100

    templated python wrapper, wrapped the HyperBuilder

--- analysis/src/_hyperstack.cxx
+++ analysis/src/_hyperstack.cxx
@@ -7,11 +7,13 @@
 #include <stdexcept>
 
 #include "HistBuilder.hh"
+#include "HyperBuilder.hh"
 #include "HistBuilderFlags.hh"
 
 static unsigned parse_flags(const char* flags);
 
-static PyObject* py_stacksusy(PyObject *self, PyObject *args)
+template<typename T>
+static PyObject* py_analysis_alg(PyObject *self, PyObject *args)
 {
   const char* input_file = ""; 
   PyObject* bits = 0; 
@@ -19,14 +21,14 @@ static PyObject* py_stacksusy(PyObject *self, PyObject *args)
   const char* flags = ""; 
 
   bool ok = PyArg_ParseTuple
-    (args,"sOs|s:stacksusy", 
+    (args,"sOs|s:algo", 
      &input_file, &bits, &output_file, &flags); 
   if (!ok) return NULL;
 
   unsigned bitflags = parse_flags(flags); 
 
   try { 
-    HistBuilder hist_builder(input_file, bitflags); 
+    T builder(input_file, bitflags); 
 
     const int n_bits = PyList_Size(bits); 
     if (PyErr_Occurred()) return NULL; 
@@ -36,11 +38,11 @@ static PyObject* py_stacksusy(PyObject *self, PyObject *args)
       unsigned mask = 0; 
       ok = PyArg_ParseTuple(entry, "sk:parsemask", &name, &mask); 
       if (!ok) return NULL; 
-      hist_builder.add_cut_mask(name, mask); 
+      builder.add_cut_mask(name, mask); 
     }
     
-    hist_builder.build(); 
-    hist_builder.save(output_file); 
+    builder.build(); 
+    builder.save(output_file); 
   }
   catch (std::runtime_error e) { 
     PyErr_SetString(PyExc_RuntimeError, e.what()); 
@@ -54,8 +56,10 @@ static PyObject* py_stacksusy(PyObject *self, PyObject *args)
 }
 
 static PyMethodDef methods[] = {
-  {"_stacksusy", py_stacksusy, METH_VARARGS, 
+  {"_stacksusy", py_analysis_alg<HistBuilder>, METH_VARARGS, 
    "don't ask, read the source"},
+  {"_hypersusy", py_analysis_alg<HyperBuilder>, METH_VARARGS, 
+   "eat a failure sandwich"}, 
   {NULL, NULL, 0, NULL}   /* sentinel */
 };
 
