f1f480d3b0f9dae15a0b86dd8611c14217239127
Dan Guest <dguest@cern.ch>
2012-11-06 18:35:26 +0100

    cutflow moves on to the met cut

--- distiller/dumpinfo.py
+++ distiller/dumpinfo.py
@@ -6,7 +6,7 @@ from itertools import islice
 ro_file = TFile('output_skim.root')
 tree = ro_file.Get('evt_tree')
 
-print '{evt:>10} {njet:>10} {pt:>10} {eta:>10} {phi:>10} {met:>10}'.format(
+print 'dan {evt:>10} {njet:>10} {pt:>10} {eta:>10} {phi:>10} {met:>10}'.format(
     evt='evt n', 
     njet='n jets', 
     pt='j1 pt', 
@@ -22,12 +22,12 @@ bits = (
     1 << 6 | # clean
     1 << 7 | # good vx
     1 << 8 | # leading jet
-    # 1 << 9 | # met
+    1 << 9 | # met
     # 1 << 10| # n jet
     0
     )
 
-forstr = '{evt:10} {njet:10} {pt:10.3f} {eta:10.3f} {phi:10.3f} {met:10.3f}'
+forstr = 'dan {evt:10} {njet:10} {pt:10.3f} {eta:10.3f} {phi:10.3f} {met:10.3f}'
 
 for entry in islice(tree, 100000): 
     if not ( (entry.pass_bits & bits) == bits): continue
