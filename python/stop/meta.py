ee234d98503aac76b3bd95356aa1bd007217674f
Dan Guest <dguest@cern.ch>
2013-01-24 17:50:55 +0100

    some work on meta

--- python/stop/meta.py
+++ python/stop/meta.py
@@ -1,6 +1,7 @@
 
 from os.path import isfile
 import cPickle
+from pyAMI import get_dataset_info
 
 class Dataset(object): 
     """
@@ -14,11 +15,15 @@ class Dataset(object):
 
         self.n_raw_entries = 0
         self.total_xsec = -1
+        self.n_expected_entries = 0
 
         self.d3pds = []
-        self.physics_type = ''
         self.skim_path = ''
         self.hist_path = ''
+
+        self.physics_type = ''
+        self.meta_source = ''
+        self.full_name = ''
 
     def corrected_xsec(self): 
         assert all(x >= 0 for x in [self.total_xsec, self.kfactor, self.filteff])
@@ -73,19 +78,77 @@ class MetaFactory(object):
         list of datasets to map out. If a list entry has two values, the 
         second is the physics type
         """
-        pass
+        self._datasets = DatasetCache()
+        self.mc_tag = 'mc12_8TeV'
+        self.p_tag = 'p1181'
+        self.datatype_tag = 'SUSY'
+
+        for entry in ds_list: 
+            non_comment_fields = entry.split('#')[0].strip().split()
+            ds_name = non_comment_fields[0]
+            dataset = Dataset()
+            dataset.name = ds_name
+            self._datasets[ds_name] = dataset
 
     def lookup_susy(self, susy_file): 
         """
         Matches whatever datasets have a cross section listed in the susy file.
         """
-        pass 
+        id_line = ''
+        for line in susy_file: 
+
+            # first line of the susy textfile is some kind of 
+            # field description         
+            if not id_line: 
+                id_line = line
+                continue
+
+            line = line.split('#')[0].strip()
+            if not line: 
+                continue
+            spl = line.split()
+            name = spl[0]
+            if name in self._datasets: 
+                ds = self._datasets[name]
+                ds.id = int(spl[0])
+                ds.name = spl[1]
+                ds.total_xsec = float(spl[2])
+                ds.kfactor = float(spl[3])
+                ds.filteff = float(spl[4])
+                ds.meta_source = 'susy textfile'
+                self._datasets[name] = ds # TODO: is this line needed???
     
-    def lookup_signal(self, signal_file): 
+    def lookup_ami(self, client): 
         """
-        Matches some other lookup file format. 
+        Matches ami
         """
-        pass
+        for ds_key, ds in self._datasets.iteritems(): 
+            wildcarded = r'{mc}%{key}%{datatype}%{p_tag}/'.format(
+                mc=self.mc_tag, key=ds.name, datatype=self.datatype_tag, 
+                p_tag=self.p_tag)
+            match_sets = query.get_datasets(wildcarded)
+            if len(match_sets) != 1: 
+                raise LookupError(
+                    'match problem with {}, matches: {}'.format(
+                        ds_key, match_sets))
+            full_name = match_sets[0]
+            info = query.get_dataset_info(client, full_name)
+            ds.kfactor = 1.0
+            try: 
+                ds.filteff = info.extra['GenFiltEff_mean']
+            except KeyError: 
+                ds.filteff = info.extra['approx_GenFiltEff']
+
+            try: 
+                ds.total_xsec = info.extra['crossSection_mean']
+            except KeyError:
+                ds.total_xsec = info.extra['approx_crossSection']
+
+            ds.n_expected_entries = info.info['totalEvents']
+
+            ds.full_name = full_name
+            ds.meta_source = 'ami'
+            
 
     def write_meta(self, output_file_name): 
         """
--- scripts/susy-lookup-meta.py
+++ scripts/susy-lookup-meta.py
@@ -12,13 +12,23 @@ client.read_config(AMI_CONFIG)
 
 print AMI_CONFIG
 
-dataset = 'mc11_7TeV.125206.PowHegPythia_VBFH130_tautauhh.evgen.EVNT.e893'
+dataset = 'mc12_8TeV.157872.MadGraphPythia_AUET2BCTEQ6L1_SM_TT_directCC_175_170.merge.NTUP_SUSY.e1355_a159_a171_r3549_p1032'
+#'mc11_7TeV.125206.PowHegPythia_VBFH130_tautauhh.evgen.EVNT.e893'
 info = get_dataset_info(client, dataset)
 nfiles = info.info['nFiles']
 total_events = info.info['totalEvents']
-filt_eff = info.extra['GenFiltEff_mean']
-xsec = info.extra['crossSection_mean']
+try: 
+   filt_eff = info.extra['GenFiltEff_mean']
+except KeyError: 
+   filt_eff = info.extra['approx_GenFiltEff']
+
+
+try: 
+   xsec = info.extra['crossSection_mean']
+except KeyError:
+   xsec = info.extra['approx_crossSection']
 
 print 'files: {}, evt: {}, filteff: {}, xsec: {}'.format(
    nfiles, total_events, filt_eff, xsec)
                                                          
+print info 
