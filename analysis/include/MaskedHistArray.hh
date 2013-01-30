bb48c98bcb351a131d6675510c175dde212a29d3
Dan Guest <dguest@cern.ch>
2013-01-30 16:45:32 +0100

    added anti-mask and weight to MaskedHistArray

--- analysis/include/MaskedHistArray.hh
+++ analysis/include/MaskedHistArray.hh
@@ -2,7 +2,7 @@
 #define MASKED_HIST_ARRAY_H
 
 #include <vector>
-#include <map>
+#include <set>
 #include <utility>
 #include <string> 
 
@@ -13,31 +13,50 @@ namespace H5 {
 
 #include "Histogram.hh"
 
+class MaskedHist: public Histogram
+{
+public: 
+  MaskedHist(const Histogram& base_hist, unsigned mask, std::string name, 
+	     unsigned anti = 0); 
+  template<typename T> 
+  void fill_mask(T values, unsigned mask, double w = 1.0); 
+  void write_to(H5::CommonFG& out_file, std::string stub = "", 
+		std::string tag = "") const;
+private: 
+  unsigned m_mask; 
+  std::string m_name; 
+  unsigned m_anti; 
+}; 
+
 class MaskedHistArray
 {
 public: 
   MaskedHistArray(const Histogram& base_hist, std::string tag = ""); 
-  void add_mask(unsigned bitmask, std::string name = ""); 
-  template<typename T> void fill(T values, const unsigned mask); 
-  std::map<std::string,Histogram> get_hists() const; 
+  void add_mask(unsigned bitmask, std::string name = "", unsigned anti = 0); 
+  template<typename T> void fill(T values, unsigned mask, double w = 1.0); 
+  // std::map<std::string,Histogram> get_hists() const; 
   void write_to(H5::CommonFG& out_file, std::string stub = "") const; 
 private: 
-  bool has_bit(unsigned) const; 
   Histogram m_base_hist; 
-  typedef std::vector<std::pair<unsigned, Histogram> > Hists;
+  typedef std::vector<MaskedHist> Hists;
   Hists m_hists; 
-  typedef std::map<unsigned, std::string> Names;
-  Names m_bit_to_name; 
+  std::set<std::string> m_names; 
   std::string m_physics_tag; 
 }; 
 
 template<typename T>
-void MaskedHistArray::fill(T values, const unsigned mask) { 
+void MaskedHist::fill_mask(T values, unsigned mask, double weight) { 
+  bool pass_mask = ( (m_mask & mask) == m_mask); 
+  bool pass_anti = ( (m_anti & mask) == 0); 
+  if (pass_mask && pass_anti) { 
+    fill(values, weight); 
+  }
+}
+
+template<typename T>
+void MaskedHistArray::fill(T values,  unsigned mask, double weight) { 
   for (Hists::iterator itr = m_hists.begin(); itr != m_hists.end(); itr++){ 
-    const unsigned hist_mask = itr->first; 
-    if ( (hist_mask & mask) == hist_mask) { 
-      itr->second.fill(values); 
-    }
+    itr->fill_mask(values, mask, weight); 
   }
 }
 
--- analysis/src/MaskedHistArray.cxx
+++ analysis/src/MaskedHistArray.cxx
@@ -8,6 +8,30 @@
 #include <string> 
 #include <cassert> 
 
+MaskedHist::MaskedHist(const Histogram& base_hist, unsigned mask, 
+		       std::string name, unsigned anti): 
+  Histogram(base_hist), 
+  m_mask(mask), 
+  m_name(name), 
+  m_anti(anti)
+{
+}
+void MaskedHist::write_to(H5::CommonFG& out_file, std::string stub, 
+			  std::string tag) const { 
+  std::string name = stub; 
+  name.append(m_name); 
+  Histogram::write_to(out_file, name); 
+  
+  if (tag.size()) { 
+    using namespace H5; 
+    DataSet the_ds = out_file.openDataSet(name); 
+    StrType vls_type(0, H5T_VARIABLE);
+    DataSpace att_space(H5S_SCALAR);
+    Attribute tag = the_ds.createAttribute("tag", vls_type, att_space); 
+    tag.write(vls_type, &tag); 
+  }
+}
+
 MaskedHistArray::MaskedHistArray(const Histogram& base_hist, 
 				 std::string tag): 
   m_base_hist(base_hist), 
@@ -15,44 +39,32 @@ MaskedHistArray::MaskedHistArray(const Histogram& base_hist,
 {
 }
 
-void MaskedHistArray::add_mask(unsigned bitmask, std::string name) 
+
+void MaskedHistArray::add_mask(unsigned bitmask, std::string name, 
+			       unsigned anti) 
 { 
-  m_hists.push_back(std::make_pair(bitmask, m_base_hist)); 
-  assert(m_bit_to_name.find(bitmask) == m_bit_to_name.end()); 
-  m_bit_to_name[bitmask] = name; 
+  m_hists.push_back(MaskedHist(m_base_hist, bitmask, name, anti)); 
+  assert(m_names.count(name) == 0); 
+  m_names.insert(name); 
 }
 
-std::map<std::string, Histogram> MaskedHistArray::get_hists() const 
-{
-  std::map<std::string, Histogram> return_map; 
-  for (Hists::const_iterator hist_itr = m_hists.begin(); 
-       hist_itr != m_hists.end(); hist_itr++) { 
-    assert(has_bit(hist_itr->first)); 
-    std::string name = m_bit_to_name.find(hist_itr->first)->second; 
-    return_map.insert(std::make_pair(name,hist_itr->second)); 
-  }
-  return return_map; 
-}
+// std::map<std::string, Histogram> MaskedHistArray::get_hists() const 
+// {
+//   std::map<std::string, Histogram> return_map; 
+//   for (Hists::const_iterator hist_itr = m_hists.begin(); 
+//        hist_itr != m_hists.end(); hist_itr++) { 
+//     assert(has_bit(hist_itr->first)); 
+//     std::string name = m_bit_to_name.find(hist_itr->first)->second; 
+//     return_map.insert(std::make_pair(name,hist_itr->second)); 
+//   }
+//   return return_map; 
+// }
 
 void MaskedHistArray::write_to(H5::CommonFG& out_file, std::string stub) 
   const { 
   for (Hists::const_iterator hist_itr = m_hists.begin(); 
        hist_itr != m_hists.end(); hist_itr++) { 
-    std::string name = stub; 
-    assert(has_bit(hist_itr->first)); 
-    name.append(m_bit_to_name.find(hist_itr->first)->second); 
-    hist_itr->second.write_to(out_file, name); 
-    if (m_physics_tag.size()) { 
-      using namespace H5; 
-      DataSet the_ds = out_file.openDataSet(name); 
-      StrType vls_type(0, H5T_VARIABLE);
-      DataSpace att_space(H5S_SCALAR);
-      Attribute tag = the_ds.createAttribute("tag", vls_type, att_space); 
-      tag.write(vls_type, &m_physics_tag); 
-    }
+    hist_itr->write_to(out_file, stub, m_physics_tag); 
   }
 }
 
-bool MaskedHistArray::has_bit(unsigned bit) const { 
-  return (m_bit_to_name.find(bit) != m_bit_to_name.end()); 
-}
