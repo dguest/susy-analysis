f33fc76332e400e13e237e8fe4ad33f9109d3020
Dan Guest <dguest@cern.ch>
2013-03-10 16:50:32 +0100

    rewrite of the python parsing routines, now uses a lot more templates

--- analysis/include/_hyperstack.hh
+++ analysis/include/_hyperstack.hh
@@ -5,6 +5,7 @@
 #include "BtagConfig.hh"
 #include "systematic_defs.hh"
 #include "RegionConfig.hh"
+#include "typedefs.hh"
 
 struct HistConfig; 
 
@@ -13,12 +14,56 @@ struct HistConfig;
 #include <vector> 
 
 static bool fill_regions(PyObject* dict, std::vector<RegionConfig>* regions); 
+static bool fill_region_essential(PyObject* dict, RegionConfig* region); 
 static bool fill_region(PyObject* dict, RegionConfig* region); 
-static bool fill_jettag(PyObject* list, std::vector<btag::JetTag>*); 
 
+template <typename T>
+static bool require(PyObject* dict, std::string key, T& dest); 
+template <typename T>
+static bool copy(PyObject* dict, std::string key, T& dest); 
+
+template <typename T>
+static bool safe_copy(PyObject* list, std::vector<T>& dest); 
+static bool safe_copy(PyObject* value, std::string& dest); 
+static bool safe_copy(PyObject* value, ull_t& dest); 
+static bool safe_copy(PyObject* value, double& dest); 
 static bool safe_copy(PyObject* value, btag::JetTag& dest); 
 static bool safe_copy(PyObject* value, syst::Systematic& dest); 
-static bool safe_copy(PyObject* value, std::string& dest); 
 
+// --- implementation ---
+
+template<typename T>
+static bool safe_copy(PyObject* list, std::vector<T>& dest) { 
+  const int n_values = PyList_Size(list); 
+  if (PyErr_Occurred()) return NULL; 
+  for (int val_n = 0; val_n < n_values; val_n++) { 
+    PyObject* entry = PyList_GetItem(list, val_n); 
+    T object;
+    if (!safe_copy(entry, object)) return false; 
+    dest.push_back(object); 
+  }
+  return true; 
+}
+
+
+template<typename T>
+static bool require(PyObject* dict, std::string key, T& dest) {
+  PyObject* string = PyDict_GetItemString(dict, key.c_str()); 
+  if (!string) { 
+    std::string prob = "regions must have specified '" + key + "'"; 
+    PyErr_SetString(PyExc_ValueError,prob.c_str()); 
+    return false; 
+  }
+  if (!safe_copy(string, dest)) return false; 
+  return true; 
+}
+
+template<typename T>
+static bool copy(PyObject* dict, std::string key, T& dest) {
+  PyObject* string = PyDict_GetItemString(dict, key.c_str()); 
+  if (!string) return true; 	// it's not required
+  if (!safe_copy(string, dest)) return false; 
+  return true; 
+}
 
 #endif 
