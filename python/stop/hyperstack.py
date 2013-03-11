ad3d2c192088c63cfad40fe450e4416871150933
Dan Guest <dguest@cern.ch>
2013-03-11 11:08:06 +0100

    stacksusy: replace some back compatibility stuff with more documentation and more helpful exceptions

--- python/stop/hyperstack.py
+++ python/stop/hyperstack.py
@@ -2,50 +2,23 @@ from _hyperstack import _stacksusy
 # from _hyperstack import _hypersusy, _cutflow
 from os.path import basename, splitext
 
-GeV = 1e3
-
-_config_opts = dict(
-    leading_jet = 260*GeV, 
-    met         = 180*GeV, 
-    j2_anti_b   = -0.2, 
-    j2_anti_u   =  2.5, 
-    j3_anti_b   = -0.2, 
-    j3_anti_u   = -0.5, 
-    btag_config = 'LOOSE_TIGHT', 
-)
-
-class Region(dict): 
-    """
-    Python wrapper for info to define a signal region. 
-    """
-    def __init__(self, init_tuple=None, tag_requirements=[]): 
-        self.jet_tag_requirements = tag_requirements
-        if not init_tuple: 
-            return None
-        try: 
-            name, required, veto = init_tuple
-        except ValueError: 
-            name, required = init_tuple
-            veto = 0
-        self.name = name
-        self.required_bits = required
-        self.veto_bits = veto
-        self.output_name = 'stupid.h5'
-    def get_dict(self): 
-        """
-        Produces the configuration info needed for _stacksusy
-        """
-        out_dict = {}
-        for name, value in self.__dict__.items(): 
-            if isinstance(value, int): 
-                out_dict[name] = long(value)
-            else: 
-                out_dict[name] = value
-        return out_dict
-
 def stacksusy(input_file, region_list, flags=''): 
     """
-    Runs the analysis on a distilled ntuple. 
+    Runs the analysis on a distilled ntuple. The region_list contains 
+    dictionaries, formatted as follows. 
+
+    Required: 
+        - name: name of region 
+        - output_name: file to save region in
+        - type: {control, signal, validation}
+
+    Optional: 
+        - required_bits: bitmask 
+        - veto_bits: bitmask
+        - systematic: enum, default NONE
+        - leading_jet_pt: cut on leading jet pt
+        - met: cut on met
+        - jet_tag_requirements: list of enums
 
     Flags: 
         v: verbose
@@ -54,13 +27,13 @@ def stacksusy(input_file, region_list, flags=''):
     """
     if not region_list: 
         raise ValueError("must give a regions list")
-    if isinstance(region_list[0], tuple): 
-        newlist = []
-        for tup in region_list: 
-            newlist.append(Region(tup, ['NOTAG']).get_dict())
-        region_list = newlist
 
-    _stacksusy(input_file, region_list, flags)
+    try: 
+        _stacksusy(input_file, region_list, flags)
+    except ValueError as e: 
+        verb_error = (
+            '{}, see stacksusy function help for details'.format(str(e)))
+        raise ValueError(verb_error)
 
 def hypersusy(input_file, mask_list, output_file='', flags='i', 
               limit_dict={}): 
