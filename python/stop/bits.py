e3423ac1179f4ba8e5680d5564861bc1c9acd3e0
Dan Guest <dguest@cern.ch>
2013-03-18 22:56:46 +0100

    updated bitdefs

--- python/stop/bits.py
+++ python/stop/bits.py
@@ -6,24 +6,26 @@ Here be bitdefs.
        cutflow. 
 """
 bits = [
-    ("grl",                  1 <<  0),
-    ("trigger",              1 <<  1),
-    ("lar_error",            1 <<  2),
-    ("core",                 1 <<  3),
-    ("jet_clean",            1 <<  4),
-    ("vxp_gt_4trk",          1 <<  5),
-    ("leading_jet",          1 <<  6),
-    ("met",                  1 <<  7),
-    ("n_jet",                1 <<  8),
-    ("dphi_jetmet_min",      1 <<  9),
-    ("lepton_veto",          1 << 10),
-    ("ctag_mainz",           1 << 11),
-
-    ('mttop'     ,           1 << 21), 
-    ('j2_anti_b' ,           1 << 22), 
-    ('j2_anti_u' ,           1 << 23), 
-    ('j3_anti_b' ,           1 << 24), 
-    ('j3_anti_u' ,           1 << 25), 
+    ("grl",  1 <<  0), 
+    ("trigger",  1 <<  1), 
+    ("lar_error",  1 <<  2), 
+    ("core",  1 <<  3), 
+    ("jet_clean",  1 <<  4), 
+    ("vxp_gt_4trk",  1 <<  5), 
+    ("leading_jet",  1 <<  6), 
+    ("met",  1 <<  7), 
+    ("n_jet",  1 <<  8), 
+    ("dphi_jetmet_min",  1 <<  9), 
+    ("electron_veto",  1 << 10), 
+    ("muon_veto",  1 << 11), 
+    ("ctag_mainz",  1 << 12), 
+    ("control_muon",  1 << 15), 
+    ("control_electron",  1 << 16),    
+    ("mttop",  1 << 21), 
+    ("j2_anti_b_opt",  1 << 22), 
+    ("j2_anti_u_opt",  1 << 23), 
+    ("j3_anti_b_opt",  1 << 24), 
+    ("j3_anti_u_opt",  1 << 25), 
     ]
 
 _bdict = dict(bits)
@@ -38,14 +40,15 @@ composite_bits = [
      _bdict['grl'] | _bdict['trigger'] | _bdict['lar_error'] | 
      _bdict['core'] | #this was set wrong in the distiller
      _bdict['jet_clean'] | _bdict['vxp_gt_4trk'] | 
-     _bdict['n_jet'] | _bdict['dphi_jetmet_min'] )
+     _bdict['n_jet'] | _bdict['dphi_jetmet_min'] ), 
+    ('lepton_veto', _bdict['electron_veto'] | _bdict['muon_veto'])
     ]
      
 _compdict = dict(composite_bits)
 
 pretag_bits = (
     'pretag', _compdict['kinematic'] | _compdict['preselection'] | 
-    _bdict['lepton_veto'])
+    _compdict['lepton_veto'])
     
 final_dict = {
     'pretag':pretag_bits, 
