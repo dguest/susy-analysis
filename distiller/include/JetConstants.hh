1d4ad09a353a36d2d71b018e078794bb032d1d85
Dan Guest <dguest@cern.ch>
2013-04-04 15:07:03 +0200

    move jet eta cut to 10 (that's what Jan is doing)

--- distiller/include/JetConstants.hh
+++ distiller/include/JetConstants.hh
@@ -4,7 +4,7 @@
 #include "constants.hh"
 
 static const float JET_PT_CUT = 20*GeV; 
-static const float JET_ETA_CUT = 2.8; 
+static const float JET_ETA_CUT = 10; 
 static const float JET_TAGGING_ETA_LIM = 2.5; 
 static const float SIGNAL_JET_PT_CUT = 30*GeV; 
 static const float UNCALIBRATED_JET_PT_MIN = 2.0*GeV; 
