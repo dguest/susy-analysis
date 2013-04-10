d73a932bdd521ccbfa69137bd5e0837e302fd8e7
Dan Guest <dguest@cern.ch>
2013-04-10 20:10:14 +0200

    more unit testing

--- distiller/src/unit_tests.cxx
+++ distiller/src/unit_tests.cxx
@@ -5,7 +5,11 @@
 
 std::pair<size_t,size_t> test_overlap_removal() { 
   const std::vector<float> dr_values = {0.1,0.2,0.4, 0.8}; 
-  const std::vector<float> test_etas = {0.05, 0.15, 0.3, 0.6, 1.0}; 
+  const std::vector<float> test_etas_abs = {0.05, 0.15, 0.3, 0.6, 1.0}; 
+  std::vector<float> test_etas(test_etas); 
+  for (auto itr = test_etas_abs.begin(); itr != test_etas_abs.end(); itr++) { 
+    test_etas.push_back(-(*itr)); 
+  }
   size_t total_survival = 0; 
   size_t total_expected = 0; 
   for (auto dr = dr_values.cbegin(); dr != dr_values.cend(); dr++) { 
@@ -19,7 +23,7 @@ std::pair<size_t,size_t> test_overlap_removal() {
       TLorentzVector* v1 = new TLorentzVector; 
       v1->SetPtEtaPhiE(1.0,*eta, 0, 0); 
       altered.push_back(v1); 
-      if (*eta > *dr) n_expected++; 
+      if (fabs(*eta) > *dr) n_expected++; 
 
       TLorentzVector* v2 = new TLorentzVector; 
       v2->SetPtEtaPhiE(1.0,*eta, *dr, 0); 
@@ -29,7 +33,7 @@ std::pair<size_t,size_t> test_overlap_removal() {
       TLorentzVector* v3 = new TLorentzVector; 
       v3->SetPtEtaPhiE(1.0, 0, *eta, 0); 
       altered.push_back(v3); 
-      if (*eta > *dr) n_expected++; 
+      if (fabs(*eta) > *dr) n_expected++; 
     }
     auto survived = remove_overlaping(masks, altered, *dr); 
     size_t n_survived = survived.size(); 
