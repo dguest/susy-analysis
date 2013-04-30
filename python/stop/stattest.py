99c14e1fedf5351b47e9ef1f108fe91fd9d7f5b5
Dan Guest <dguest@cern.ch>
2013-04-30 23:21:11 +0200

    added binomial_interval

--- python/stop/stattest.py
+++ python/stop/stattest.py
@@ -1,4 +1,4 @@
-from scipy.stats import chi2
+from scipy.stats import chi2, beta
 from math import erf
 
 def poisson_interval(points, alpha=erf(1/2**0.5)): 
@@ -7,3 +7,6 @@ def poisson_interval(points, alpha=erf(1/2**0.5)):
     low, high = (chi2.ppf(a/2, 2*k) / 2, chi2.ppf(1-a/2, 2*(k + 1)) / 2)
     low[k == 0] = 0.0
     return low, high
+
+def binomial_interval(x, n, alpha=(1-erf(1/2**0.5))): 
+    return beta.ppf(alpha/2, x, n - x - 1), beta.ppf(alpha/2, x + 1, n - x)
