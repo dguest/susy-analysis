e631f3ccf76aff3ce5b28747da32c964966a5dc2
Dan Guest <dguest@cern.ch>
2013-05-17 21:09:16 +0200

    fix dumbness in efficiency error fucntion

--- python/stop/stattest.py
+++ python/stop/stattest.py
@@ -14,15 +14,8 @@ def binomial_interval(x, n, alpha=(1-erf(1/2**0.5))):
     high = beta.ppf(1-alpha/2, x + 1, n - x) * n
     return low, high
 
-def _valid_delta(n, wt2): 
-    out = np.empty(n.shape)
-    out[:] = float('nan')
-    mask = wt2 > 0.0
-    out[mask] = n[mask] / wt2[mask]**0.5
-    return out
-
 def efficiency_error(p, f, p_wt2, f_wt2): 
-    delta_p = _valid_delta(p, p_wt2)
-    delta_f = _valid_delta(f, f_wt2)
+    delta_p = p_wt2**0.5
+    delta_f = f_wt2**0.5
     error = ( (delta_p * f)**2 + (delta_f * p)**2 )**0.5 / (p + f)**2.0
     return error
