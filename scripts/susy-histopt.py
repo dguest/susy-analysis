4eb5a3754e5bf20480f1b10abcfd1deaa179b263
Dan Guest <dguest@cern.ch>
2013-06-11 20:41:11 +0200

    some hacking around histopt

--- scripts/susy-histopt.py
+++ scripts/susy-histopt.py
@@ -2,13 +2,17 @@
 import yaml, itertools
 # from ROOT import kBlack,kWhite,kGray,kRed,kPink,kMagenta,kViolet,kBlue,kAzure,kCyan,kTeal,kGreen,kSpring,kYellow,kOrange
 from stop.hists import HistNd
+from stop.bullshit import OutputFilter
 import h5py
 import os, sys
 from os.path import isfile, isdir, join
 from collections import defaultdict
 
 class CountDict(dict): 
-    def __init__(self, kin_dir, region): 
+    """
+    Stores as (syst, physics, region)
+    """
+    def __init__(self, kin_dir, quick=False): 
 
         self.systematics = []
         for d in os.listdir(kin_dir): 
@@ -16,6 +20,8 @@ class CountDict(dict):
                 self.systematics.append(d)
 
         for syst in self.systematics: 
+            if quick and syst not in ['baseline', 'jer']: 
+                continue
             tmp = defaultdict(dict)
             print 'loading {}'.format(syst)
             agg_path = join(kin_dir, syst, 'aggregate.h5')
@@ -24,19 +30,18 @@ class CountDict(dict):
             with h5py.File(agg_path, 'r') as h5file: 
                for phys, var_group in h5file.iteritems(): 
                    for var, reg_group in var_group.iteritems():
-                       hist = reg_group[region]
-                       if var == 'kinematics': 
-                           tmp[syst, phys]['sum'] = HistNd(hist)
-                       elif var == 'kinematicWt2': 
-                           tmp[syst, phys]['wt2'] = HistNd(hist)
+                       for region, hist in reg_group.iteritems(): 
+                           if var == 'kinematics': 
+                               tmp[syst, phys, region]['sum'] = HistNd(hist)
+                           elif var == 'kinematicWt2': 
+                               tmp[syst, phys, region]['wt2'] = HistNd(hist)
             self.update(tmp)
 
 def run(): 
     inf = float('inf')
-    ext = CountDict(sys.argv[1], 'anti0')
-    print ext['baseline','ttbar']['sum']['met'].slice(70000.0, inf)['leadingJetPt'].slice(100000.0, inf)
+    counts = CountDict(sys.argv[1], quick=True)
+    print counts['baseline','data', 'ttbar0']['sum']['met'].slice(70000.0, inf)['leadingJetPt'].slice(150000.0, inf)
     
-def other(): 
     import ROOT
     with OutputFilter(): 
         hf = ROOT.RooStats.HistFactory
@@ -46,36 +51,39 @@ def other():
 
     # ****** here be HistFactory ******
 
-    fit_config = FitConfig('stack.yml')
-    print '--- loading counts ---'
-    fit_config.load_counts()
-    print 'done'
     # Create the measurement
     meas = hf.Measurement("meas", "meas")
 
-    meas.SetOutputFilePrefix("./results/example")
-    meas.SetPOI("c1norm")
-    meas.AddConstantParam("Lumi")
-    for syst in fit_config.systematics: 
+    result_dir = 'results'
+    if not isdir(result_dir): 
+        os.mkdir(result_dir)
+    meas.SetOutputFilePrefix(join(result_dir,'example'))
+    meas.SetPOI("mu_SIG")
+    # meas.AddConstantParam("Lumi")
+    for syst in ['jer']: 
         meas.AddConstantParam('alpha_{}'.format(syst))
 
     meas.SetLumi(1.0)
     meas.SetLumiRelErr(lumiError)
     meas.SetExportOnly(False)
 
+    def cut_hist(hist): 
+        d_cut = (150000.0,inf)
+        return hist['met'].slice(*d_cut)['leadingJetPt'].slice(*d_cut)
+
     # Create the channel
-    for cr in fit_config.control_regions: 
+    for cr in ['ttbar0']: 
         chan = hf.Channel(cr)
-        chan.SetData(fit_config.counts['NONE']['data'][cr])
+        chan.SetData(cut_hist(counts['baseline','data',cr]['sum']))
         chan.SetStatErrorConfig(0.05, "Poisson")
 
-        for bg in fit_config.backgrounds: 
+        for bg in ['ttbar']:
             background = hf.Sample('_'.join([cr,bg]))
-            base_count = fit_config.counts['NONE'][bg][cr]['normed']
+            base_count = cut_hist(counts['baseline',bg,cr]['sum'])
             background.SetValue(base_count)
             background.AddNormFactor(bg, 1,0,10)
 
-            for syst in fit_config.systematics: 
+            for syst in ['jer']:
                 if syst in {'JES', 'U','C','B','T'}: 
                     syst_up = fit_config.counts[syst + 'UP'][bg][cr]
                     sup_normed = syst_up['normed']
@@ -86,9 +94,8 @@ def other():
                         syst, sup_normed / base_count, 
                         sdn_normed / base_count)
                 else: 
-                    syst_counts = fit_config.counts[syst][bg][cr]
-                    syst_normed = syst_counts['normed']
-                    rel_syst = syst_normed / base_count - 1
+                    syst_counts = cut_hist(counts[syst,bg,cr]['sum'])
+                    rel_syst = syst_counts / base_count - 1
                     background.AddOverallSys(
                         syst, 1 - rel_syst/2, 1 + rel_syst/2)
 
@@ -99,7 +106,8 @@ def other():
     # Now, do the measurement
     ROOT.gROOT.SetBatch(True)
     with OutputFilter(accept_re='ERROR:'): 
-        workspace = hf.MakeModelAndMeasurementFast(meas)
+        pass
+    workspace = hf.MakeModelAndMeasurementFast(meas)
     print '------------ fitting -------------------'
     with OutputFilter(): 
         ROOT.RooStats.HistFactory.FitModel(workspace)
