8b8757edae0ad0c36487ed3d7a5fceb3906ffa82
Dan Guest <dguest@cern.ch>
2013-06-21 17:21:36 +0200

    added susy count checking utility

--- scripts/susy-util.py
+++ scripts/susy-util.py
@@ -21,6 +21,10 @@ _cutflow_help="""
 Gets the aggregated cutflow from lots of files. 
 """
 
+_check_help="""
+Check meta file for missing events
+"""
+
 import argparse, sys
 from os.path import isdir, isfile, join, expanduser, splitext
 from os.path import dirname, basename, abspath
@@ -41,7 +45,8 @@ def run():
         'meta':list_meta_info, 
         'hadd':hadd, 
         'rename':rename, 
-        'cutflow':cutflow}
+        'cutflow':cutflow, 
+        'check':check}
     subs[config.which](config)
 
 
@@ -103,7 +108,36 @@ def get_config():
     # cutflow stuff
     cutflow = subs.add_parser('cutflow', description=_cutflow_help)
     cutflow.add_argument('count_files', nargs='+')
+
+    # check meta
+    check = subs.add_parser('check', description=_check_help)
+    check.add_argument('meta_file')
+
     return parser.parse_args(sys.argv[1:])
+
+def check(config): 
+    type_expected_counter = Counter()
+    type_found_counter = Counter()
+    
+    ds_meta = meta.DatasetCache(config.meta_file)
+    for key, ds in ds_meta.iteritems(): 
+        expected = ds.n_expected_entries
+        found = ds.n_raw_entries
+        if expected != found: 
+            temp_str = 'sample {} ({}): expected {:,}, found {:,}\n'
+            sys.stderr.write(temp_str.format(
+                    ds.key, ds.physics_type, expected, found))
+        type_found_counter[ds.physics_type] += found
+        type_expected_counter[ds.physics_type] += expected
+
+    nameslen = max(len(n) for n in type_expected_counter) + 1
+    explen = max(len(str(n)) for n in type_expected_counter.values()) + 1
+    for phys_type in type_expected_counter: 
+        expected = type_expected_counter[phys_type]
+        found = type_found_counter[phys_type]
+        print '{:{}}: {:{te}} of {:{te}} ({:.2%})'.format(
+            phys_type, nameslen, found, expected, float(found)/expected, 
+            te=explen)
 
 def cutflow(config): 
     cut_counts = Counter()
