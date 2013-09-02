73445d4563472a8e344cc4ceca6dbe133865db41
Dan Guest <dguest@cern.ch>
2013-09-02 19:52:23 +0200

    exclusion plane excludes

--- scripts/susy-exclusion.py
+++ scripts/susy-exclusion.py
@@ -10,14 +10,19 @@ import yaml
 import h5py
 import numpy as np
 import bisect
+from os.path import dirname
 
-def _load_plotters(): 
-    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-    from matplotlib.figure import Figure
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.axes_grid1 import make_axes_locatable
-    import matplotlib as mp
-    from stop.style import ax_labels, vdict, hdict
+from stop.bullshit import make_dir_if_none
+from stop.style import vdict, hdict
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigCanvas
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from stop.style import vdict
+from scipy.interpolate import Rbf
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
 
 def get_args(): 
     d = 'default: %(default)s'
@@ -25,7 +30,9 @@ def get_args():
     pl_parent.add_argument(
         '-e','--plot-ext', help='output extension', choices={'.pdf','.png'}, 
         default='.pdf')
-    parser = argparse.ArgumentParser(description=__doc__)
+    pl_parent.add_argument('-o','--out-dir', default='exclusion')
+
+    parser = argparse.ArgumentParser(description=__doc__, parents=[pl_parent])
     parser.add_argument('hdf_input')
     parser.add_argument('yaml_cuts')
     return parser.parse_args(sys.argv[1:])
@@ -59,6 +66,7 @@ def run():
     hdf = h5py.File(args.hdf_input)
     with open(args.yaml_cuts) as cuts_yml: 
         cuts_dict = yaml.load(cuts_yml)
+    ex_plane = ExclusionPlane()
     for sr_name, sp_group in hdf.iteritems(): 
         stop_lsp_ul = []
         sr_cuts = cuts_dict[sr_name]
@@ -66,7 +74,81 @@ def run():
             part, mstop, mlsp = _get_part_mstop_mlsp(sp_name)
             upper_limit = _value_from_plane(plane, sr_cuts)
             stop_lsp_ul.append( (mstop, mlsp, upper_limit) )
+
+        out_name = os.path.join(args.out_dir, sr_name + args.plot_ext)
+        ex_plane.add_config(stop_lsp_ul, sr_name)
+        # _plot_points(stop_lsp_ul, out_name)
+    ex_plane.save(os.path.join(args.out_dir, 'comp' + args.plot_ext))
+
+class ExclusionPlane(object): 
+    def __init__(self): 
+        self.figure = Figure(figsize=(9,8))
+        self.canvas = FigCanvas(self.figure)
+        self.ax = self.figure.add_subplot(1,1,1)
+        self.ax.tick_params(labelsize=16)
+        self.ax.set_ylabel('$m_{\mathrm{lsp}}$ [GeV]', **vdict)
+        self.ax.set_xlabel('$m_{\mathrm{stop}}$ [GeV]', **hdict)
+        self.color_itr = iter('rgbm')
+        self._proxy_contour = []
+        self.lw = 3
+        self._pts = None
         
+        
+    def add_config(self, stop_lsp_ul, label): 
+        x, y, z = zip(*stop_lsp_ul)
+        xmin = 0.0
+        ymin = 0.0
+        xi = np.linspace(xmin, max(x), 100)
+        yi = np.linspace(ymin, max(y), 100)
+        xp, yp = np.meshgrid(xi, yi)
+        delta = xp - yp
+        mask = (delta < 0) | ( delta > 80.4)
+        rbf = Rbf(x, y, z, function='linear')
+        zp = rbf(xp, yp)
+        zp[mask] = 3
+        extent = [xmin, max(x), ymin,max(y)]
+        ct_color = next(self.color_itr)
+        draw_opts = dict(color=ct_color, linewidth=self.lw)
+        ct = self.ax.contour(xp, yp, zp, [1.0], 
+                             colors=ct_color, linewidths=self.lw )
+        self._proxy_contour.append(
+            ( Line2D((0,0),(0,1), **draw_opts), str(label)) )
+        if not self._pts: 
+            self._pts = self.ax.plot(x,y,'.k')
+
+    def save(self, name): 
+        self.ax.legend(
+            *zip(*self._proxy_contour), fontsize='xx-large', 
+             loc='upper left', framealpha=0.0)
+        make_dir_if_none(dirname(name))
+        self.canvas.print_figure(name, bbox_inches='tight')
+        
+
+def _plot_points(stop_lsp_ul, out_name): 
+    figure = Figure(figsize=(9,8))
+    canvas = FigCanvas(figure)
+    ax = figure.add_subplot(1,1,1)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", "5%", pad="1.5%")
+    x, y, z = zip(*stop_lsp_ul)
+    xi = np.linspace(min(x), max(x), 100)
+    yi = np.linspace(min(y), max(y), 100)
+    xp, yp = np.meshgrid(xi, yi)
+    delta = xp - yp
+    mask = (delta < 0) | ( delta > 80.4)
+    rbf = Rbf(x, y, z, function='linear')
+    zp = rbf(xp, yp)
+    zp[mask] = 3
+    extent = [min(x), max(x), min(y),max(y)]
+    im = ax.imshow(zp, vmin=0.0, vmax=2.0, origin='lower',
+                   extent=extent)
+    cb = plt.colorbar(im,cax=cax)
+    ct = ax.contour(xp, yp, zp, [1.0])
+    ax.plot(x,y,'o')
+
+    make_dir_if_none(dirname(out_name))
+
+    canvas.print_figure(out_name, bbox_inches='tight')
 
 if __name__ == '__main__': 
     run()
