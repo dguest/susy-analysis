975741719f342df764c2e2cb53221f6054b97447
Dan Guest <dguest@cern.ch>
2013-10-25 20:48:24 +0200

    remove dphi cut from the preselection bits

--- python/stop/bits.py
+++ python/stop/bits.py
@@ -26,8 +26,7 @@ composite_bits = [
     ('preselection', 
      _bdict['grl'] | _bdict['trigger'] | _bdict['lar_error'] | 
      _bdict['core'] | #this was set wrong in the distiller
-     _bdict['jet_clean'] | _bdict['vxp_gt_4trk'] | 
-     _bdict['dphi_jetmet_min'] ), 
+     _bdict['jet_clean'] | _bdict['vxp_gt_4trk'] ), 
     ('lepton_veto', _bdict['electron_veto'] | _bdict['muon_veto'])
     ]
      
