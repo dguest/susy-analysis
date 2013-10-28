e5872ba5785e5bc962dbb8663ee87e92bcc3d4a5
Dan Guest <dguest@cern.ch>
2013-10-28 20:15:56 +0100

    change the number of signal jets on which the dphi(met, jet) cut is applied from 3 to 4

--- analysis/include/constants_stopcuts.hh
+++ analysis/include/constants_stopcuts.hh
@@ -1,7 +1,11 @@
 #ifndef CONSTANTS_STOPCUTS_HH
 #define CONSTANTS_STOPCUTS_HH
 
+// standard susy QCD cut
 static const float MIN_DPHI_JET_MET = 0.4; 
-static const size_t DPHI_JET_MET_NJET = 3; 
+
+// both monojet and c-tagged group seem to confirm we should use 
+// only the first 4 jets
+static const size_t DPHI_JET_MET_NJET = 4; 
 
 #endif 
