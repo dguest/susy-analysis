cdd0a8e2df178b9955181ded67f3ddebfa1d5b59
Dan Guest <dguest@cern.ch>
2014-01-20 07:22:35 +0100

    add check for input file

--- skim/src/run-skim.cxx
+++ skim/src/run-skim.cxx
@@ -3,13 +3,16 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <stdexcept>
 
 // thin wrapper for the Skimmer class
 int main(int narg, char* argv[]) { 
   std::vector<std::string> variables; 
-  std::ifstream var_file("branches.txt"); 
+  std::string varf = "branches.txt"; 
+  std::ifstream var_file(varf.c_str()); 
 
   std::string line; 
+  if (!var_file.is_open()) throw std::runtime_error("can't read " + varf);
   while (!var_file.eof()) { 
     var_file >> line; 
     variables.push_back(line); 
