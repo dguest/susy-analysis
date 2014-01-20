167e2744cf4d1f1e4fe3c921fc464810e531588f
Dan Guest <dguest@cern.ch>
2014-01-20 22:42:33 +0100

    work on skimming script

--- scripts/susy-grid-prune.py
+++ scripts/susy-grid-prune.py
@@ -3,7 +3,7 @@
 """
 Script to submit prun jobs to skim D3PDs. 
 
-Creates a skimmer.py in the current directory. 
+copies the `skimmer` directory into the current one
 """
 
 import sys, os, re, shutil
@@ -24,8 +24,9 @@ def _strip_front(ds_name):
         return _strip_front('.'.join(ds_name.split('.')[1:]))
     return ds_name
 
-def submit_ds(ds_name, debug=False, version=0, used_vars='used_vars.txt', 
-              out_talk=None, in_tar=None, maxgb=False, blacklist=[]): 
+def submit_ds(ds_name, debug=False, version=0, branches='branches.txt', 
+              out_talk=None, in_tar=None, maxgb=False, blacklist=[], 
+              skim_dir='skim-stuff'): 
 
     user = os.path.expandvars('$USER')
     preskim_name = _strip_front(ds_name)
@@ -34,14 +35,15 @@ def submit_ds(ds_name, debug=False, version=0, used_vars='used_vars.txt',
     out_ds = 'user.{user}.{in_ds}.skim_p{rev}_v{version}/'.format(
         user=user, in_ds=output_base, version=version, rev=rev_number)
 
-    run_string = 'python skimmer.py {} %IN'.format(used_vars)
+    bstring = 'cd {sd}; make; cd ..; ln {sd}/run-skim'.format(sd=skim_dir)
+    run_string = 'run-skim %IN'
 
     input_args = [
         '--inDS=' + ds_name,
         '--outDS=' + out_ds,
-        '--outputs=skim-output.root,set-branches.txt,n-entries.txt', 
+        '--outputs=skim-output.root', 
         '--excludeFile=*.tar,*.log,*.sh,*.out,*.root',
-        '--extFile={}'.format(used_vars), 
+        '--extFile={}'.format(branches), 
         '--rootVer=5.34/07', 
         '--cmtConfig=x86_64-slc5-gcc43-opt', 
         ]
@@ -51,11 +53,10 @@ def submit_ds(ds_name, debug=False, version=0, used_vars='used_vars.txt',
         input_args.append('--inTarBal={}'.format(in_tar))
     if maxgb: 
         input_args.append('--nGBPerJob={}'.format(maxgb))
-
-    exec_string = run_string
-
     
-    submit_string = ['prun','--exec',exec_string] + input_args
+    submit_string = [
+        'prun','--bexec', bstring, 
+        '--exec', run_string] + input_args
     if debug:
         submit_string = ['echo'] + submit_string
     ps = Popen(submit_string, stdout=PIPE, stderr=PIPE)
@@ -65,76 +66,22 @@ def submit_ds(ds_name, debug=False, version=0, used_vars='used_vars.txt',
         out_talk.put(head_line + err + out + '\n')
     return out_ds, out, err
 
-_skimmer = r"""
-import sys
-from ROOT import TChain, TFile, TTree, gDirectory, gROOT
-gROOT.SetBatch()
-
-branches = open(sys.argv[1])
-
-susy_chain = TChain('{chain_name}')
-collection_chain = TChain('CollectionTree')
-
-out_file = TFile('skim-output.root', 'recreate')
-
-for f in sys.argv[2].split(','): 
-    print 'adding', f
-    ret = susy_chain.Add(f,-1)
-    ret = collection_chain.Add(f,-1)
-
-set_branches = open('set-branches.txt','w')
-entries = susy_chain.GetEntries()
-susy_chain.Draw('0.5 >> sum_hist(1,0,1)','mcevt_weight[0][0]')
-try: 
-    wt_entries = gDirectory.Get('sum_hist').GetBinContent(1)
-except AttributeError: 
-    wt_entries = 0.0
-
-entries_file = open('n-entries.txt','w')
-entries_file.write(str(entries) + '\n')
-entries_file.write(str(wt_entries) + '\n')
-entries_file.close()
-
-if not entries: 
-    set_branches.write('none\n')
-    print 'chain is empty, quitting...'
-    sys.exit(0)
-
-susy_chain.SetBranchStatus('*',0)
-for branch in branches: 
-    print 'setting', branch.strip()
-    susy_chain.SetBranchStatus(branch.strip(), 1)
-    set_branches.write(branch.strip() + '\n')
-set_branches.close()
-
-out_susy = susy_chain.CopyTree('{all_the_cuts}')
-out_collection = collection_chain.CloneTree()
-
-out_file.WriteTObject(out_susy)
-out_file.WriteTObject(out_collection)
-"""
-
 class LocalSkimmer(object): 
-    script_name = 'skimmer.py'
-    def __init__(self, vars_file, all_the_cuts='', tarball=None, 
-                 chain='susy'): 
-        self.all_the_cuts = all_the_cuts
-        self.vars_file = vars_file
+    def __init__(self, branches, tarball=None, script_dir='skim-stuff'): 
+        self.vars_file = branches
         self.tarball = tarball
-        self.chain = chain
+        self.script_dir = script_dir
     def __enter__(self): 
-        if os.path.isfile(self.script_name): 
-            os.remove(self.script_name)
-        self.write()
+        if os.path.islink(self.script_dir): 
+            os.remove(self.script_dir)
+        self.link()
         if self.tarball: 
             self.tar()
         self.current_files = set(os.listdir('.'))
-    def write(self): 
-        with open(self.script_name,'w') as sk: 
-            sk.write(_skimmer.format(
-                    all_the_cuts=self.all_the_cuts,
-                    chain_name=self.chain, 
-                    branches_file=self.vars_file))
+    def link(self): 
+        path_to_skim = os.path.join(__file__,'..','skim')
+        abs_path = os.path.abspath(path_to_skim)
+        os.symlink(abs_path, self.script_dir)
         return self
     def tar(self): 
         user = os.path.expandvars('$USER')
@@ -184,73 +131,6 @@ def get_config(config_name, varlist_name):
         config.set('cuts', 'met_low', '70000')
         config.set('cuts','mark_skim', 'true')
     return config
-
-def get_cuts_and_vars(cuts_dict): 
-    if cuts_dict.get('mark_skim','false') == 'true': 
-        return get_marks_skim(cuts_dict)
-    cuts = {}
-    variables = []
-    if 'met_low' in cuts_dict: 
-        cuts['met_low'] = 'MET_RefFinal_et > {}'.format(
-            cuts_dict['met_low'])
-        variables.append('MET_RefFinal_et')
-    if 'triggers' in cuts_dict: 
-        triggers = cuts_dict['triggers'].split()
-        for trig in triggers: 
-            variables.append(trig)
-        cuts['triggers'] = '( {} )'.format(' || '.join(triggers))
-    if 'trigger' in cuts_dict: 
-        trigger = cuts_dict['trigger']
-        variables.append(trigger)
-        cuts['trigger'] = trigger
-    if 'n_staco' in cuts_dict: 
-        cuts['n_staco'] = 'mu_staco_n >= {}'.format(cuts_dict['n_staco'])
-
-    ored_cuts = set(cuts_dict.get('or','').split())
-    all_cuts = set(cuts.keys())
-    anded_cuts = all_cuts - ored_cuts - set(['or'])
-
-    or_cuts = [cuts[x] for x in ored_cuts]
-    and_cuts = [cuts[x] for x in anded_cuts]
-    if or_cuts: 
-        or_str = ' || '.join(or_cuts)
-        and_cuts.append('( {} )'.format(or_str))
-    return ' && '.join(and_cuts), variables
-
-def get_marks_skim(cuts_dict): 
-    met_final = 'MET_Egamma10NoTau_RefFinal'
-    met_muon = 'MET_Egamma10NoTau_Muon_Total_Staco'
-    triggers = [
-        'EF_xe80_tclcw_tight',  # main trigger (should work all year)
-        'EF_mu24i_tight',          # for Z CR
-        ]
-    met_final_exp = '{f}_etx * {f}_etx + {f}_ety * {f}_ety'.format(
-        f=met_final)
-    met_muon_exp = (
-        '({f}_etx - {m}_etx)*({f}_etx - {m}_etx) + ' 
-        '({f}_ety - {m}_ety)*({f}_ety - {m}_ety)' 
-        ).format(f=met_final, m=met_muon)
-    met_final_str = '(sqrt({}) > {})'.format(
-        met_final_exp, cuts_dict['met_low'])
-    met_muon_str = '(sqrt({}) > {})'.format(
-        met_muon_exp, cuts_dict['met_low'])
-    trig_or = ' || '.join(triggers)
-    met_or = ' {} || {} '.format(met_final_str, met_muon_str)
-    branches = triggers + [
-        '{}_et{}'.format(m,c) for m in [met_final, met_muon] for c in 'xy']
-    final = '({}) && ({})'.format(trig_or, met_or)
-
-    return final, branches
-
-def add_vars(new_vars, file_name): 
-    """
-    adds new_vars to end of file 'file_name' if they aren't there already
-    """
-    with open(file_name, 'r+') as f: 
-        all_vars = set(x.strip() for x in f.readlines())
-        missing_vars = set(new_vars) - all_vars
-        for var in missing_vars: 
-            f.write(var + '\n')
 
 class Reporter(Process): 
     """
@@ -389,41 +269,28 @@ if __name__ == '__main__':
             config.write(cfg)
         sys.exit('wrote config, exiting')
 
-    cuts, new_variables = get_cuts_and_vars(dict(config.items('cuts')))
-
-    chain_name = config.get('variables','chain')
     if args.get_slimmer: 
-        LocalSkimmer(args.ds_list, all_the_cuts=cuts, 
-                     chain=chain_name).write()
+        LocalSkimmer(branches).write()
         sys.exit('got skimmer, exiting...')
 
     if not find_executable('prun'): 
         raise OSError("prun isn't defined, you need to set it up")
 
     version = config.getint('version', 'version')
-    if args.increment_version: 
-        version += 1
-        config.set('version','version',str(version))
-
-    used_vars = args.variable_list
-    if not used_vars: 
-        used_vars = config.get('variables','varlist')
-    add_vars(new_variables, used_vars)
-
-    if not os.path.isfile(used_vars): 
-        sys.exit('you need {}!'.format(used_vars))
+    tarball = 'jobtar.tar'
+    branches = 'branches.txt'
+    if not os.path.isfile(branches): 
+        sys.exit('you need {}!'.format(branches))
 
     datasets = _get_datasets(args.ds_list)
-
     output_container_list = open(args.out_name,'w')
     out_log = open(args.full_log,'w')
-    tarball = 'jobtar.tar'
 
     reporter = Reporter(len(datasets), out_log)
     reporter.start()
     def submit(ds): 
         return submit_ds(ds, args.debug, version, 
-                         used_vars=used_vars, 
+                         branches=branches, 
                          out_talk=reporter.queue, 
                          in_tar=tarball, 
                          maxgb=args.ngb, 
@@ -435,7 +302,7 @@ if __name__ == '__main__':
             return pool.map_async(sub, dss).get(999999)
     else: 
         map_func = map    
-    with LocalSkimmer(used_vars,cuts,tarball=tarball,chain=chain_name): 
+    with LocalSkimmer(branches=branches, tarball=tarball): 
         out_tuples = map_func(submit, datasets)
     reporter.close()
     output_datasets = [ds for ds, out, err in out_tuples]
