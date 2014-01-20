726bd48b2878b72da3497b09a32017aa9e6b5050
Dan Guest <dguest@cern.ch>
2014-01-20 07:22:06 +0100

    fixed linkdef

--- skim/include/LinkDef.h
+++ skim/include/LinkDef.h
@@ -1,23 +1,20 @@
-
 #ifdef __CINT__
-
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
+#pragma link C++ nestedclasses;
 
-#pragma link C++ nestedclass; 
-
-#pragma link C++ class std::vector<std::vector<int> >+; 
-#pragma link C++ class std::vector<std::vector<unsigned int> >+;
-#pragma link C++ class std::vector<std::vector<long> >+;
-#pragma link C++ class std::vector<std::vector<unsigned long> >+;
-#pragma link C++ class std::vector<std::vector<float> >+;
+#pragma link C++ class vector<vector<int> >+;
+#pragma link C++ class vector<vector<unsigned int> >+;
+#pragma link C++ class vector<vector<long> >+;
+#pragma link C++ class vector<vector<unsigned long> >+;
 #pragma link C++ class vector<vector<float> >+;
-#pragma link C++ class std::vector<std::vector<double> >+;
-#pragma link C++ class std::vector<std::string>+;
-#pragma link C++ class std::vector<std::vector<std::string> >+;
-#pragma link C++ class std::vector<float>+;
-#pragma link C++ class std::map<std::string, int> >+; 
+#pragma link C++ class vector<vector<float> >+;
+#pragma link C++ class vector<vector<double> >+;
+#pragma link C++ class vector<string>+;
+#pragma link C++ class vector<vector<string> >+;
+#pragma link C++ class vector<float>+;
+#pragma link C++ class map<string, int> >+;
 
 
 #endif // __CINT__
