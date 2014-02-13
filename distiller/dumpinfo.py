90f9477433fa275f6d774a162125536f20ec228f
Dan Guest <dguest@cern.ch>
2014-02-12 23:18:02 -0500

    minor

--- distiller/dumpinfo.py
+++ distiller/dumpinfo.py
@@ -3,7 +3,9 @@
 from ROOT import TFile, TTree
 from itertools import islice
 
-ro_file = TFile('py-output.root')
+ro_file = TFile('egamma-skim.root')
 total = ro_file.Get('total_events').GetVal()
+skimmed = ro_file.Get('skimmed_events').GetVal()
 print "total {}".format(total)
+print "skimmed {}".format(skimmed)
 
