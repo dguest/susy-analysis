bb6524c32a4220ead6285e3d0da8affded1841b8
Dan Guest <dguest@cern.ch>
2014-02-13 13:12:51 -0500

    stuff to dump skim info

--- distiller/dumpinfo.py
+++ distiller/dumpinfo.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python2.7
 
 from ROOT import TFile, TTree
-from itertools import islice
+import sys
 
-ro_file = TFile('egamma-skim.root')
-total = ro_file.Get('total_events').GetVal()
-skimmed = ro_file.Get('skimmed_events').GetVal()
-print "total {}".format(total)
-print "skimmed {}".format(skimmed)
+def run(): 
+    if len(sys.argv) == 1: 
+        sys.exit('need files')
+    for fname in sys.argv[1:]: 
+        dump(fname)
+    
+def dump(file_name): 
+    from ROOT import TFile, TTree
+    ro_file = TFile(file_name)
+    total = ro_file.Get('total_events').GetVal()
+    skimmed = ro_file.Get('skimmed_events').GetVal()
+    print "total {}".format(total)
+    print "skimmed {}".format(skimmed)
 
+
+if __name__ == '__main__': 
+    run()
