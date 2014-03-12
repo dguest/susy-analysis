363f42ef53a4ef2a9eb8b519ee508801d49681e2
Dan Guest <dguest@cern.ch>
2014-03-12 18:36:07 -0400

    rework trigger logic

--- analysis/src/QualityEventSelection.cxx
+++ analysis/src/QualityEventSelection.cxx
@@ -20,12 +20,13 @@ QualityEventSelection::~QualityEventSelection() {
 bool QualityEventSelection::pass(const EventObjects& obj) const { 
   const EventRecoParameters& reco = obj.reco;
   using namespace reg;
-  bool one_lep_trigger = trig::pass_single_lepton_trigger(reco, m_stream);
-  bool two_lep_trigger = trig::pass_two_lepton_trigger(reco, m_stream);
   auto n_lep = reco.n_veto_electrons + reco.n_veto_muons;
   if (n_lep == 0) {
     return reco.pass_met_trigger;
-  } else if (n_lep == 1) {
+  }
+  bool one_lep_trigger = trig::pass_single_lepton_trigger(reco, m_stream);
+  bool two_lep_trigger = trig::pass_two_lepton_trigger(reco, m_stream);
+  if (n_lep == 1) {
     return one_lep_trigger;
   } else if (n_lep == 2) {
     return two_lep_trigger;
--- analysis/src/trigger_logic.cxx
+++ analysis/src/trigger_logic.cxx
@@ -29,40 +29,33 @@ namespace trig {
   // trigger for the W and t control regions
   bool pass_single_lepton_trigger(const EventRecoParameters& reco, 
 				  reg::Stream stream){
+    bool mu_trig = reco.pass_single_mu_trigger;
+    bool el_trig = reco.pass_single_el_trigger;
+    if (stream == reg::Stream::SIMULATED) { 
+      return mu_trig || el_trig;
+    }
     if (stream == reg::Stream::MUON) { 
-      if (! reco.pass_single_mu_trigger ) return false; 
+      return mu_trig && !el_trig;
     } else if (stream == reg::Stream::ELECTRON ) { 
-      if (! reco.pass_single_el_trigger ) return false;
+      return el_trig;
     }
-    if (stream != reg::Stream::SIMULATED) { 
-      // stream overlap removal
-      if ( reco.pass_single_el_trigger && reco.pass_single_mu_trigger ) { 
-	// if the event fires both triggers, we only take the one from the 
-	// electron stream
-	if (stream == reg::Stream::MUON) return false;
-      }
-    }
-    return true;
+    throw std::invalid_argument("got unknown stream in lepton trig check");
   }
 
   // trigger for the Z cr
   bool pass_two_lepton_trigger(const EventRecoParameters& reco,
 			       reg::Stream stream){
+    bool mu_trig = reco.pass_two_mu_trigger;
+    bool el_trig = reco.pass_two_el_trigger;
+    if (stream == reg::Stream::SIMULATED) { 
+      return mu_trig || el_trig;
+    }
     if (stream == reg::Stream::MUON) { 
-      if (! reco.pass_two_mu_trigger) return false;
+      return mu_trig && !el_trig;
     } else if (stream == reg::Stream::ELECTRON ) { 
-      if (! reco.pass_two_el_trigger) return false;
+      return el_trig;
     }
-
-    if (stream != reg::Stream::SIMULATED) { 
-      // stream overlap removal
-      if ( reco.pass_two_el_trigger && reco.pass_two_mu_trigger ) { 
-	// if the event fires both triggers, we only take the one from the 
-	// electron stream
-	if (stream == reg::Stream::MUON) return false;
-      }
-    }
-    return true;
+    throw std::invalid_argument("got unknown stream in 2 lep trig check");
   }
 
 }
