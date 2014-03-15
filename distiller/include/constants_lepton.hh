47b0565fe1e1815550ea0b2ac01fbd692a1cbc35
Dan Guest <dguest@cern.ch>
2014-03-15 14:34:57 -0400

    lower signal electron pt to 20 GeV (puts the cut burdon on the trigger-matching)

--- distiller/include/constants_lepton.hh
+++ distiller/include/constants_lepton.hh
@@ -13,7 +13,7 @@ static const float SIGNAL_ELECTRON_D0_CUT = 1.0;
 static const float SIGNAL_ELECTRON_Z0_CUT = 2.0; 
 
 const static float CONTROL_ELECTRON_REL_ISOLATION = 0.1; 
-const static float CONTROL_ELECTRON_PT = 25e3; 
+const static float CONTROL_ELECTRON_PT = 20e3; 
 
 static const float MUON_PT_CUT = 6*GeV; 
 static const float MUON_ETA_CUT = 2.4; 
