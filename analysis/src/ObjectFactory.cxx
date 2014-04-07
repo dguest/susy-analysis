e646bc3d85dce3b6a8542e3b39053d9dde99efa7
Dan Guest <dguest@cern.ch>
2014-04-07 17:42:39 -0400

    whitespace

--- analysis/src/ObjectFactory.cxx
+++ analysis/src/ObjectFactory.cxx
@@ -9,245 +9,245 @@
 #include "common_functions.hh"
 #include "StackerExceptions.hh"
 
-#include <string> 
-#include <vector> 
-#include <set> 
-#include <stdexcept> 
+#include <string>
+#include <vector>
+#include <set>
+#include <stdexcept>
 #include <algorithm>
 
 #include "TVector2.h"
 #include "TFile.h"
 #include "TTree.h"
 
-namespace { 
-  void set_reco_parameters(TTree*, EventRecoParameters&); 
+namespace {
+  void set_reco_parameters(TTree*, EventRecoParameters&);
 }
 
-MetBuffer::MetBuffer(TTree* tree, const std::string& prefix) { 
-  int errors = 0; 
+MetBuffer::MetBuffer(TTree* tree, const std::string& prefix) {
+  int errors = 0;
   errors += std::abs(
-    tree->SetBranchAddress((prefix + "met").c_str(), &met)); 
+    tree->SetBranchAddress((prefix + "met").c_str(), &met));
   errors += std::abs(
-    tree->SetBranchAddress((prefix + "met_phi").c_str(), &met_phi)); 
-  if (errors) throw std::logic_error("unknown met prefix: " + prefix); 
+    tree->SetBranchAddress((prefix + "met_phi").c_str(), &met_phi));
+  if (errors) throw std::logic_error("unknown met prefix: " + prefix);
 }
 
 
-ObjectFactory::ObjectFactory(const std::string& root_file, int n_jets) : 
-  m_tree(0), 
-  m_file(0), 
-  m_event_reco_parameters(0), 
-  m_hfor_type(-1), 
-  m_leading_cjet_pos(-1), 
-  m_subleading_cjet_pos(-1), 
-  m_mc_event_weight(0.0/0.0), 
-  m_pileup_weight(0.0/0.0), 
-  m_boson_pt_weight(0.0/0.0), 
-  m_boson_pt(0.0/0.0), 
-  m_ioflags(0), 
-  m_evt_sf(0), 
+ObjectFactory::ObjectFactory(const std::string& root_file, int n_jets) :
+  m_tree(0),
+  m_file(0),
+  m_event_reco_parameters(0),
+  m_hfor_type(-1),
+  m_leading_cjet_pos(-1),
+  m_subleading_cjet_pos(-1),
+  m_mc_event_weight(0.0/0.0),
+  m_pileup_weight(0.0/0.0),
+  m_boson_pt_weight(0.0/0.0),
+  m_boson_pt(0.0/0.0),
+  m_ioflags(0),
+  m_evt_sf(0),
   m_skim_counts(0)
 
 {
-  m_file = new TFile(root_file.c_str()); 
-  if (!m_file) { 
-    throw std::runtime_error(root_file + " could not be found"); 
+  m_file = new TFile(root_file.c_str());
+  if (!m_file) {
+    throw std::runtime_error(root_file + " could not be found");
   }
-  m_skim_counts = new SkimCounts(*m_file); 
-  m_tree = dynamic_cast<TTree*>(m_file->Get("evt_tree")); 
-  if (!m_tree) { 
-    throw std::runtime_error("evt_tree could not be found"); 
+  m_skim_counts = new SkimCounts(*m_file);
+  m_tree = dynamic_cast<TTree*>(m_file->Get("evt_tree"));
+  if (!m_tree) {
+    throw std::runtime_error("evt_tree could not be found");
   }
-  m_met.emplace(syst::NONE, new MetBuffer(m_tree, "")); 
+  m_met.emplace(syst::NONE, new MetBuffer(m_tree, ""));
 
-  m_event_reco_parameters = new EventRecoParameters; 
-  set_reco_parameters(m_tree, *m_event_reco_parameters); 
+  m_event_reco_parameters = new EventRecoParameters;
+  set_reco_parameters(m_tree, *m_event_reco_parameters);
 
-  try { 
-    set_branch(m_tree,"hfor_type", &m_hfor_type); 
-    set_branch(m_tree,"truth_leading_cjet_pos", &m_leading_cjet_pos); 
-    set_branch(m_tree,"truth_subleading_cjet_pos", &m_subleading_cjet_pos); 
-    set_branch(m_tree,"mc_event_weight", &m_mc_event_weight); 
-    set_branch(m_tree,"pileup_weight", &m_pileup_weight); 
-    m_evt_sf = new EventScalefactors(m_tree); 
-  } catch (MissingBranch& err) { 
-    m_ioflags |= ioflag::no_truth; 
+  try {
+    set_branch(m_tree,"hfor_type", &m_hfor_type);
+    set_branch(m_tree,"truth_leading_cjet_pos", &m_leading_cjet_pos);
+    set_branch(m_tree,"truth_subleading_cjet_pos", &m_subleading_cjet_pos);
+    set_branch(m_tree,"mc_event_weight", &m_mc_event_weight);
+    set_branch(m_tree,"pileup_weight", &m_pileup_weight);
+    m_evt_sf = new EventScalefactors(m_tree);
+  } catch (MissingBranch& err) {
+    m_ioflags |= ioflag::no_truth;
   }
 
-  // set_branch(m_tree,"htx", &m_htx); 
-  for (int ji = 0; ji < n_jets; ji++) { 
-    std::string base_name = "jet" + std::to_string(ji) + "_"; 
-    m_jet_buffers.push_back(new JetBuffer); 
-    m_jet_buffers.back()->set_buffer(m_tree, base_name); 
+  // set_branch(m_tree,"htx", &m_htx);
+  for (int ji = 0; ji < n_jets; ji++) {
+    std::string base_name = "jet" + std::to_string(ji) + "_";
+    m_jet_buffers.push_back(new JetBuffer);
+    m_jet_buffers.back()->set_buffer(m_tree, base_name);
   }
-  if (m_tree->GetBranch("truth_boson_pt_weight")) { 
-    m_tree->SetBranchAddress("truth_boson_pt_weight", &m_boson_pt_weight); 
-    m_tree->SetBranchAddress("truth_boson_pt", &m_boson_pt); 
-    m_ioflags |= ioflag::has_boson_pt_weight; 
+  if (m_tree->GetBranch("truth_boson_pt_weight")) {
+    m_tree->SetBranchAddress("truth_boson_pt_weight", &m_boson_pt_weight);
+    m_tree->SetBranchAddress("truth_boson_pt", &m_boson_pt);
+    m_ioflags |= ioflag::has_boson_pt_weight;
   }
-  set_branch(m_tree, "boson_child_pt", &m_boson_child_pt); 
-  set_branch(m_tree, "boson_child_phi", &m_boson_child_phi); 
-} 
+  set_branch(m_tree, "boson_child_pt", &m_boson_child_pt);
+  set_branch(m_tree, "boson_child_phi", &m_boson_child_phi);
+}
 
-ObjectFactory::~ObjectFactory() 
+ObjectFactory::~ObjectFactory()
 {
   for (auto itr: m_jet_buffers) {
-    delete itr; 
-    itr = 0; 
+    delete itr;
+    itr = 0;
   }
-  delete m_tree; 
-  if (m_file) { 
-    m_file->Close("R"); 
+  delete m_tree;
+  if (m_file) {
+    m_file->Close("R");
   }
-  delete m_file; 
-  delete m_event_reco_parameters; 
-  delete m_evt_sf; 
-  m_file = 0; 
-  m_evt_sf = 0; 
-  for (auto itr: m_met) { 
-    delete itr.second; 
-    itr.second = 0; 
+  delete m_file;
+  delete m_event_reco_parameters;
+  delete m_evt_sf;
+  m_file = 0;
+  m_evt_sf = 0;
+  for (auto itr: m_met) {
+    delete itr.second;
+    itr.second = 0;
   }
-  delete m_skim_counts; 
+  delete m_skim_counts;
 }
 
-int ObjectFactory::entries() const { 
-  return m_tree->GetEntries(); 
+int ObjectFactory::entries() const {
+  return m_tree->GetEntries();
 }
 
-bool ObjectFactory::is_data() const { 
-  return ioflag::no_truth & m_ioflags; 
+bool ObjectFactory::is_data() const {
+  return ioflag::no_truth & m_ioflags;
 }
 
-void ObjectFactory::entry(int n) { 
-  m_tree->GetEntry(n); 
+void ObjectFactory::entry(int n) {
+  m_tree->GetEntry(n);
 }
 
-std::vector<Jet> ObjectFactory::jets() const { 
+std::vector<Jet> ObjectFactory::jets() const {
 
-  std::vector<Jet> jets_out; 
-  int jrank = 0; 
-  for (auto jet_buffer: m_jet_buffers) { 
-    if (!jet_buffer->has_jet()) { 
-      return jets_out; 
+  std::vector<Jet> jets_out;
+  int jrank = 0;
+  for (auto jet_buffer: m_jet_buffers) {
+    if (!jet_buffer->has_jet()) {
+      return jets_out;
     }
-    jets_out.push_back(Jet(jet_buffer,m_ioflags)); 
-    Jet& jet = *jets_out.rbegin(); 
-    jet.set_rank(jrank); 
-    jet.set_event_met(met()); 
-    jrank++; 
+    jets_out.push_back(Jet(jet_buffer,m_ioflags));
+    Jet& jet = *jets_out.rbegin();
+    jet.set_rank(jrank);
+    jet.set_event_met(met());
+    jrank++;
   }
 
-  return jets_out; 
+  return jets_out;
 }
 
-TVector2 ObjectFactory::met(syst::Systematic sy) const  { 
-  if (!m_met.count(sy)) sy = syst::NONE; 
-  const MetBuffer& buf = *m_met.at(sy); 
-  TVector2 met; 
-  met.SetMagPhi(buf.met, buf.met_phi); 
-  return met; 
+TVector2 ObjectFactory::met(syst::Systematic sy) const  {
+  if (!m_met.count(sy)) sy = syst::NONE;
+  const MetBuffer& buf = *m_met.at(sy);
+  TVector2 met;
+  met.SetMagPhi(buf.met, buf.met_phi);
+  return met;
 }
-EventRecoParameters ObjectFactory::event_reco_parameters() const { 
-  return *m_event_reco_parameters; 
+EventRecoParameters ObjectFactory::event_reco_parameters() const {
+  return *m_event_reco_parameters;
 }
 int ObjectFactory::leading_cjet_pos() const {return m_leading_cjet_pos;}
 int ObjectFactory::subleading_cjet_pos() const {return m_subleading_cjet_pos;}
-double ObjectFactory::event_weight() const 
+double ObjectFactory::event_weight() const
 {
-  if (m_ioflags & ioflag::no_truth) { 
-    return 1.0; 
+  if (m_ioflags & ioflag::no_truth) {
+    return 1.0;
   }
-  float base = m_mc_event_weight * m_pileup_weight; 
-  return base; 
+  float base = m_mc_event_weight * m_pileup_weight;
+  return base;
 }
 float ObjectFactory::marks_boson_pt_weight() const {
-  if (!(m_ioflags & ioflag::has_boson_pt_weight) ) { 
-    return 1.0; 
+  if (!(m_ioflags & ioflag::has_boson_pt_weight) ) {
+    return 1.0;
   }
-  return m_boson_pt_weight; 
+  return m_boson_pt_weight;
 }
-float ObjectFactory::boson_pt() const { 
-  if (!(m_ioflags & ioflag::has_boson_pt_weight) ) { 
-    return -1.0; 
+float ObjectFactory::boson_pt() const {
+  if (!(m_ioflags & ioflag::has_boson_pt_weight) ) {
+    return -1.0;
   }
-  return m_boson_pt; 
+  return m_boson_pt;
 }
-TVector2 ObjectFactory::boson_child() const { 
-  if (m_boson_child_pt < 0.0) { 
-    return TVector2(); 
+TVector2 ObjectFactory::boson_child() const {
+  if (m_boson_child_pt < 0.0) {
+    return TVector2();
   }
-  TVector2 vec; 
-  vec.SetMagPhi(m_boson_child_pt, m_boson_child_phi); 
-  return vec; 
+  TVector2 vec;
+  vec.SetMagPhi(m_boson_child_pt, m_boson_child_phi);
+  return vec;
 }
 
-EventScalefactors* ObjectFactory::event_scalefactors() const { 
-  return m_evt_sf; 
+EventScalefactors* ObjectFactory::event_scalefactors() const {
+  return m_evt_sf;
 }
 
-const SkimCounts* ObjectFactory::skim_counts() const { 
-  return m_skim_counts; 
+const SkimCounts* ObjectFactory::skim_counts() const {
+  return m_skim_counts;
 }
 
-hfor::JetType ObjectFactory::hfor_type() const { 
-  if (m_ioflags & ioflag::no_truth) { 
-    throw std::runtime_error("no truth info found, can't get hfor type"); 
+hfor::JetType ObjectFactory::hfor_type() const {
+  if (m_ioflags & ioflag::no_truth) {
+    throw std::runtime_error("no truth info found, can't get hfor type");
   }
-  using namespace hfor; 
-  switch (m_hfor_type) { 
-  case -1: return NONE; 
-  case 0: return BB; 
-  case 1: return CC; 
-  case 2: return C; 
-  case 3: return LIGHT; 
-  case 4: return KILL; 
-  default: 
-    throw std::runtime_error("given undefined hfor type"); 
+  using namespace hfor;
+  switch (m_hfor_type) {
+  case -1: return NONE;
+  case 0: return BB;
+  case 1: return CC;
+  case 2: return C;
+  case 3: return LIGHT;
+  case 4: return KILL;
+  default:
+    throw std::runtime_error("given undefined hfor type");
   }
 }
 
-bool has_higher_pt(const Jet& v1, const Jet& v2) { 
-  return v1.Pt() > v2.Pt(); 
+bool has_higher_pt(const Jet& v1, const Jet& v2) {
+  return v1.Pt() > v2.Pt();
 }
 
 
-namespace { 
-  void set_reco_parameters(TTree* tree, EventRecoParameters& re) { 
+namespace {
+  void set_reco_parameters(TTree* tree, EventRecoParameters& re) {
 
 #define SET_PARAMETER(tree, branch)		\
   set_branch(tree, #branch, &re.branch)
 
-    SET_PARAMETER(tree, pass_met_trigger); 
-    SET_PARAMETER(tree, pass_single_el_trigger); 
-    SET_PARAMETER(tree, pass_single_mu_trigger); 
-    SET_PARAMETER(tree, pass_two_el_trigger); 
-    SET_PARAMETER(tree, pass_two_mu_trigger); 
+    SET_PARAMETER(tree, pass_met_trigger);
+    SET_PARAMETER(tree, pass_single_el_trigger);
+    SET_PARAMETER(tree, pass_single_mu_trigger);
+    SET_PARAMETER(tree, pass_two_el_trigger);
+    SET_PARAMETER(tree, pass_two_mu_trigger);
 
-    SET_PARAMETER(tree, pass_event_quality); 
+    SET_PARAMETER(tree, pass_event_quality);
 
-    SET_PARAMETER(tree, pass_osdf); 
-    SET_PARAMETER(tree, pass_ossf); 
+    SET_PARAMETER(tree, pass_osdf);
+    SET_PARAMETER(tree, pass_ossf);
 
-    SET_PARAMETER(tree, n_preselected_jets); 
-    SET_PARAMETER(tree, n_signal_jets); 
-    SET_PARAMETER(tree, n_baseline_electrons); 
-    SET_PARAMETER(tree, n_baseline_muons); 
-    SET_PARAMETER(tree, n_signal_electrons); 
-    SET_PARAMETER(tree, n_signal_muons); 
+    SET_PARAMETER(tree, n_preselected_jets);
+    SET_PARAMETER(tree, n_signal_jets);
+    SET_PARAMETER(tree, n_baseline_electrons);
+    SET_PARAMETER(tree, n_baseline_muons);
+    SET_PARAMETER(tree, n_signal_electrons);
+    SET_PARAMETER(tree, n_signal_muons);
 
-    SET_PARAMETER(tree, min_jetmet_dphi); 
-    SET_PARAMETER(tree, met_eff); 
-    SET_PARAMETER(tree, mct); 
-    SET_PARAMETER(tree, mcc); 
-    SET_PARAMETER(tree, mt); 
-    SET_PARAMETER(tree, mll); 
-    SET_PARAMETER(tree, htx); 
+    SET_PARAMETER(tree, min_jetmet_dphi);
+    SET_PARAMETER(tree, met_eff);
+    SET_PARAMETER(tree, mct);
+    SET_PARAMETER(tree, mcc);
+    SET_PARAMETER(tree, mt);
+    SET_PARAMETER(tree, mll);
+    SET_PARAMETER(tree, htx);
 
     SET_PARAMETER(tree, max_lepton_pt);
 
 #undef SET_PARAMETER
 
   }
-}  
+}
 
--- analysis/src/common_functions.cxx
+++ analysis/src/common_functions.cxx
@@ -14,19 +14,19 @@
 // ___________________________________________________________________
 // branch setting stuff
 
-void internal_set(TTree* tree, const std::string& name, 
-		  const std::string& type_name, void* var) { 
-  unsigned found = 0; 
-  tree->SetBranchStatus(name.c_str(), true, &found); 
+void internal_set(TTree* tree, const std::string& name,
+		  const std::string& type_name, void* var) {
+  unsigned found = 0;
+  tree->SetBranchStatus(name.c_str(), true, &found);
   if (found != 1) {
     throw MissingBranch(
       "" + std::to_string(found) + " branches found setting "  + name);
   }
-  std::string type = tree->GetLeaf(name.c_str())->GetTypeName(); 
-  if (type != type_name) { 
+  std::string type = tree->GetLeaf(name.c_str())->GetTypeName();
+  if (type != type_name) {
     throw std::logic_error(
-      "branch types don't match for '" + name + "': tree has " + 
-      type_name + " need a " + type); 
+      "branch types don't match for '" + name + "': tree has " +
+      type_name + " need a " + type);
   }
   tree->SetBranchAddress(name.c_str(), var);
 }
@@ -41,27 +41,27 @@ std::string get_name(int) { return "Int_t"; }
 // ___________________________________________________________________
 // mttop stuff
 
-double get_mttop(const std::vector<Jet>& jets, TVector2 met) 
+double get_mttop(const std::vector<Jet>& jets, TVector2 met)
 {
-  assert(jets.size() == 3); 
-  std::map<double, std::pair<unsigned, unsigned> > mass_pairs; 
-  for (unsigned iii = 0; iii < jets.size(); iii++) { 
-    for (unsigned jjj = 0; jjj < iii; jjj++) { 
-      double mass = (jets.at(iii) + jets.at(jjj)).Mag(); 
-      mass_pairs[mass] = std::make_pair(iii,jjj); 
+  assert(jets.size() == 3);
+  std::map<double, std::pair<unsigned, unsigned> > mass_pairs;
+  for (unsigned iii = 0; iii < jets.size(); iii++) {
+    for (unsigned jjj = 0; jjj < iii; jjj++) {
+      double mass = (jets.at(iii) + jets.at(jjj)).Mag();
+      mass_pairs[mass] = std::make_pair(iii,jjj);
     }
   }
-  std::pair<unsigned,unsigned> lowest = mass_pairs.begin()->second; 
-  for (unsigned iii = 0; iii < jets.size(); iii++) { 
-    if (iii != lowest.first && iii != lowest.second) { 
-      return get_mttop(jets.at(iii), met); 
+  std::pair<unsigned,unsigned> lowest = mass_pairs.begin()->second;
+  for (unsigned iii = 0; iii < jets.size(); iii++) {
+    if (iii != lowest.first && iii != lowest.second) {
+      return get_mttop(jets.at(iii), met);
     }
   }
-  assert(false); 
+  assert(false);
 }
-double get_mttop(const Jet& jet, TVector2 met) { 
-  TVector2 jet_pt(jet.Px(), jet.Py()); 
-  return sqrt( pow( jet_pt.Mod() + sqrt(met.Mod2() + pow(W_MASS,2) ) , 2) - 
-	       (jet_pt + met).Mod2() ); 
+double get_mttop(const Jet& jet, TVector2 met) {
+  TVector2 jet_pt(jet.Px(), jet.Py());
+  return sqrt( pow( jet_pt.Mod() + sqrt(met.Mod2() + pow(W_MASS,2) ) , 2) -
+	       (jet_pt + met).Mod2() );
 }
 
