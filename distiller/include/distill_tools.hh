99ec67290b2d60afc2da5f2bf165eea9bd65c9d7
Dan Guest <dguest@cern.ch>
2014-04-07 20:27:13 -0400

    whitespace and roundoff check for mt2

--- distiller/include/distill_tools.hh
+++ distiller/include/distill_tools.hh
@@ -1,17 +1,17 @@
 #ifndef DISTILL_TOOLS_HH
 #define DISTILL_TOOLS_HH
 
-class SUSYObjDef; 
+class SUSYObjDef;
 class FakeMetEstimator;
 class SelectedJet;
-class TVector2; 
-class BtagCalibration; 
-class TLorentzVector; 
-class Muon; 
-class Electron; 
+class TVector2;
+class BtagCalibration;
+class TLorentzVector;
+class Muon;
+class Electron;
 
 // #include "TChain.h"
-#include <vector> 
+#include <vector>
 #include <map>
 #include <cassert>
 #include <cmath>
@@ -21,50 +21,50 @@ class Electron;
 
 // --- utility functions
 
-TVector2 get_boson_child_pt(const std::vector<Electron*>&, 
-			    const std::vector<Muon*>&); 
+TVector2 get_boson_child_pt(const std::vector<Electron*>&,
+			    const std::vector<Muon*>&);
 
-float get_min_jetmet_dphi(const std::vector<SelectedJet*>&, 
-			  const TVector2& , size_t njet); 
+float get_min_jetmet_dphi(const std::vector<SelectedJet*>&,
+			  const TVector2& , size_t njet);
 
-TVector2 sum_muon_pt(const std::vector<Muon*>& muons); 
-TVector2 sum_el_pt(const std::vector<Electron*>& el); 
+TVector2 sum_muon_pt(const std::vector<Muon*>& muons);
+TVector2 sum_el_pt(const std::vector<Electron*>& el);
 
 double get_htx(const std::vector<SelectedJet*>&, const size_t x);
 
-double get_energy_weighted_time(const std::vector<SelectedJet*>&, 
-				size_t n_jets); 
+double get_energy_weighted_time(const std::vector<SelectedJet*>&,
+				size_t n_jets);
 
 // m_ct functions
-double get_m_ct(const TLorentzVector& v1, const TLorentzVector& v2); 
+double get_m_ct(const TLorentzVector& v1, const TLorentzVector& v2);
 
 double get_mctcorr(const TLorentzVector& v1, const TLorentzVector& v2,
 		   const std::vector<Electron*>& el,
 		   const std::vector<Muon*>& mu,
 		   const TVector2& vmet);
 
-double get_mt(const std::vector<Electron*>& , const std::vector<Muon*>& , 
-	      const TVector2& ); 
+double get_mt(const std::vector<Electron*>& , const std::vector<Muon*>& ,
+	      const TVector2& );
 
-double get_mll(const std::vector<Electron*>& , const std::vector<Muon*>&); 
+double get_mll(const std::vector<Electron*>& , const std::vector<Muon*>&);
 
 template<typename T>
-double scalar_sum_pt(const T& objects); 
+double scalar_sum_pt(const T& objects);
 
-void set_bit(std::vector<SelectedJet*>& jets, unsigned bit); 
+void set_bit(std::vector<SelectedJet*>& jets, unsigned bit);
 
-void calibrate_jets(std::vector<SelectedJet*> jets, const BtagCalibration*); 
+void calibrate_jets(std::vector<SelectedJet*> jets, const BtagCalibration*);
 
 
 // ---- templates -----
 
 template<typename T>
-double scalar_sum_pt(const T& obj) { 
-  double sum = 0.0; 
-  for (auto itr: obj) { 
-    sum += itr->Pt(); 
+double scalar_sum_pt(const T& obj) {
+  double sum = 0.0;
+  for (auto itr: obj) {
+    sum += itr->Pt();
   }
-  return sum; 
+  return sum;
 }
 
-#endif 
+#endif
--- distiller/src/distill_tools.cxx
+++ distiller/src/distill_tools.cxx
@@ -10,7 +10,7 @@
 #include "cutflag.hh"
 
 #include <algorithm>
-#include <string> 
+#include <string>
 // #include <streambuf>
 #include <cassert>
 #include <cstdlib> // getenv, others
@@ -19,102 +19,102 @@
 #include "TVector2.h"
 #include "mctlib.hh"
 #include <math.h>
-#include <stdexcept> 
+#include <stdexcept>
 
 
 //____________________________________________________________
-// utility functions 
+// utility functions
 
-TVector2 get_boson_child_pt(const std::vector<Electron*>& el, 
-			    const std::vector<Muon*>& mu) { 
-  const size_t n_muons = mu.size(); 
-  for (size_t mu_n = 0; mu_n < n_muons; mu_n++) { 
-    for (size_t mu_n2 = mu_n + 1; mu_n2 < n_muons; mu_n2++) { 
-      const auto& mu1 = *mu.at(mu_n); 
-      const auto& mu2 = *mu.at(mu_n2); 
-      if (are_os_zmass(mu1, mu2)) { 
-	return (mu1 + mu2).Vect().XYvector(); 
+TVector2 get_boson_child_pt(const std::vector<Electron*>& el,
+			    const std::vector<Muon*>& mu) {
+  const size_t n_muons = mu.size();
+  for (size_t mu_n = 0; mu_n < n_muons; mu_n++) {
+    for (size_t mu_n2 = mu_n + 1; mu_n2 < n_muons; mu_n2++) {
+      const auto& mu1 = *mu.at(mu_n);
+      const auto& mu2 = *mu.at(mu_n2);
+      if (are_os_zmass(mu1, mu2)) {
+	return (mu1 + mu2).Vect().XYvector();
       }
     }
   }
-  if (mu.size() == 1) { 
-    return mu.at(0)->Vect().XYvector(); 
+  if (mu.size() == 1) {
+    return mu.at(0)->Vect().XYvector();
   }
-  if (el.size() == 1) { 
-    return el.at(0)->Vect().XYvector(); 
+  if (el.size() == 1) {
+    return el.at(0)->Vect().XYvector();
   }
-  return TVector2(); 
+  return TVector2();
 }
 
-float get_min_jetmet_dphi(const std::vector<SelectedJet*>& jets, 
-			  const TVector2& met, size_t n_jets){ 
-  typedef std::vector<SelectedJet*> JVec; 
-  typedef JVec::const_iterator JItr; 
-  TLorentzVector mector; 
-  mector.SetPtEtaPhiE(1,0,met.Phi(),1); 
-  float min_dphi = M_PI; 
-  size_t lastj = std::min(n_jets, jets.size()); 
+float get_min_jetmet_dphi(const std::vector<SelectedJet*>& jets,
+			  const TVector2& met, size_t n_jets){
+  typedef std::vector<SelectedJet*> JVec;
+  typedef JVec::const_iterator JItr;
+  TLorentzVector mector;
+  mector.SetPtEtaPhiE(1,0,met.Phi(),1);
+  float min_dphi = M_PI;
+  size_t lastj = std::min(n_jets, jets.size());
   for(JItr itr = jets.begin(); itr != jets.begin() + lastj; itr++){
-    float deltaphi = fabs(mector.DeltaPhi(**itr)); 
-    min_dphi = std::min(deltaphi, min_dphi); 
+    float deltaphi = fabs(mector.DeltaPhi(**itr));
+    min_dphi = std::min(deltaphi, min_dphi);
   }
-  return min_dphi; 
+  return min_dphi;
 }
 
-TVector2 sum_muon_pt(const std::vector<Muon*>& muons) { 
-  TLorentzVector sum; 
-  for (auto mu: muons) { 
-    sum += *mu; 
+TVector2 sum_muon_pt(const std::vector<Muon*>& muons) {
+  TLorentzVector sum;
+  for (auto mu: muons) {
+    sum += *mu;
   }
-  return sum.Vect().XYvector(); 
+  return sum.Vect().XYvector();
 }
-TVector2 sum_el_pt(const std::vector<Electron*>& els) { 
-  TLorentzVector sum; 
-  for (auto el: els) { 
-    sum += *el; 
+TVector2 sum_el_pt(const std::vector<Electron*>& els) {
+  TLorentzVector sum;
+  for (auto el: els) {
+    sum += *el;
   }
-  return sum.Vect().XYvector(); 
+  return sum.Vect().XYvector();
 }
 
-double get_htx(const std::vector<SelectedJet*>& jets, const size_t x){ 
-  double htx = 0; 
-  const unsigned required_bits = jetbit::pass_susy_def; 
-  size_t n_jets = jets.size(); 
-  if (n_jets <= x) return 0.0; 
-  for (auto itr = jets.cbegin() + x; itr != jets.cend(); itr++) { 
-    unsigned jet_bits = (*itr)->bits(); 
-    bool has_required = ( (jet_bits & required_bits) == required_bits); 
-    if (has_required) { 
-      htx += (*itr)->Pt(); 
+double get_htx(const std::vector<SelectedJet*>& jets, const size_t x){
+  double htx = 0;
+  const unsigned required_bits = jetbit::pass_susy_def;
+  size_t n_jets = jets.size();
+  if (n_jets <= x) return 0.0;
+  for (auto itr = jets.cbegin() + x; itr != jets.cend(); itr++) {
+    unsigned jet_bits = (*itr)->bits();
+    bool has_required = ( (jet_bits & required_bits) == required_bits);
+    if (has_required) {
+      htx += (*itr)->Pt();
     }
   }
-  return htx; 
+  return htx;
 }
 
 double get_energy_weighted_time(const std::vector<SelectedJet*>& jets,
-				size_t max_jet) { 
+				size_t max_jet) {
   if (jets.size() == 0) return 0.0;
-  const size_t n_jets = std::min(max_jet, jets.size()); 
-  double denom = 0; 
-  double num = 0; 
-  
-  for (auto itr = jets.begin(); itr != jets.begin() + n_jets; itr++) { 
-    const auto& jet = **itr; 
-    denom += jet.E(); 
-    num += jet.E() * jet.timing(); 
+  const size_t n_jets = std::min(max_jet, jets.size());
+  double denom = 0;
+  double num = 0;
+
+  for (auto itr = jets.begin(); itr != jets.begin() + n_jets; itr++) {
+    const auto& jet = **itr;
+    denom += jet.E();
+    num += jet.E() * jet.timing();
   }
-  return num/denom; 
+  return num/denom;
 }
 
-double get_m_ct(const TLorentzVector& v1, const TLorentzVector& v2) { 
-  double et1 = v1.Pt(); 
-  double et2 = v2.Pt(); 
-  TVector2 diff_pt = v1.Vect().XYvector() - v2.Vect().XYvector(); 
-  double mct2 = std::pow(et1 + et2, 2) - std::pow(diff_pt.Mod(), 2); 
-  return std::sqrt(mct2); 
+double get_m_ct(const TLorentzVector& v1, const TLorentzVector& v2) {
+  double et1 = v1.Pt();
+  double et2 = v2.Pt();
+  TVector2 diff_pt = v1.Vect().XYvector() - v2.Vect().XYvector();
+  double mct2 = std::pow(et1 + et2, 2) - std::pow(diff_pt.Mod(), 2);
+  return std::sqrt(mct2);
 }
 
-double get_mctcorr(const TLorentzVector& tv1, const TLorentzVector& tv2, 
+double get_mctcorr(const TLorentzVector& tv1, const TLorentzVector& tv2,
 		   const std::vector<Electron*>& els,
 		   const std::vector<Muon*>& mus,
 		   const TVector2& vmet)
@@ -134,57 +134,66 @@ double get_mctcorr(const TLorentzVector& tv1, const TLorentzVector& tv2,
   return mct_object.mctcorr(v1, v2, vds, ptm, 8e6, 0.0);
 }
 
-double get_mt(const std::vector<Electron*>& el, 
-	      const std::vector<Muon*>& mu, const TVector2& met) { 
-  const double nothing = -1; 
-  size_t nel = el.size(); 
-  size_t nmu = mu.size(); 
-  // require exactly one lepton 
-  if (nel + nmu != 1) return nothing; 
-  TLorentzVector* lep = 0; 
-  if (nel > 0) { 
-    lep = el.at(0); 
-  } else if (nmu > 0) { 
-    lep = mu.at(0); 
-  } else { 
-    throw std::logic_error("lepton count in get_mt"); 
+double get_mt(const std::vector<Electron*>& el,
+	      const std::vector<Muon*>& mu, const TVector2& met) {
+  const double nothing = -1;
+  size_t nel = el.size();
+  size_t nmu = mu.size();
+  // require exactly one lepton
+  if (nel + nmu != 1) return nothing;
+  TLorentzVector* lep = 0;
+  if (nel > 0) {
+    lep = el.at(0);
+  } else if (nmu > 0) {
+    lep = mu.at(0);
+  } else {
+    throw std::logic_error("lepton count in get_mt");
   }
-  TVector2 lep2vec = lep->Vect().XYvector(); 
-  return std::sqrt(2*lep2vec.Mod()*met.Mod() - 2*lep2vec*met);
+  TVector2 lep2vec = lep->Vect().XYvector();
+  double dot_part = 2*lep2vec*met;
+  double scalar_part = 2*lep2vec.Mod()*met.Mod();
+  double mt_squared =  scalar_part - dot_part;
+  // roundoff error may get us negative mt_squared, return zero in that case
+  if (mt_squared < 0){
+    // require that it be roundoff error and not something weird...
+    assert(mt_squared / dot_part < 1e-3);
+    return 0;
+  }
+  return std::sqrt(mt_squared);
 }
 
-double get_mll(const std::vector<Electron*>& el, 
-	       const std::vector<Muon*>& mu) { 
-  const double nothing = -1; 
-  size_t nel = el.size(); 
-  size_t nmu = mu.size(); 
-  size_t total_lep = nel + nmu; 
-  if (total_lep != 2) return nothing; 
-  if (nel == 1 && nmu == 1) { 
-    return (*el.at(0) + *mu.at(0)).M(); 
-  } else if (nel == 2) { 
-    return (*el.at(0) + *el.at(1)).M(); 
-  } else if (nmu == 2) { 
-    return (*mu.at(0) + *mu.at(1)).M(); 
-  } else { 
-    throw std::logic_error(std::to_string(nel) + " electrons, " + 
-			   std::to_string(nmu) + " muons in get_mll"); 
+double get_mll(const std::vector<Electron*>& el,
+	       const std::vector<Muon*>& mu) {
+  const double nothing = -1;
+  size_t nel = el.size();
+  size_t nmu = mu.size();
+  size_t total_lep = nel + nmu;
+  if (total_lep != 2) return nothing;
+  if (nel == 1 && nmu == 1) {
+    return (*el.at(0) + *mu.at(0)).M();
+  } else if (nel == 2) {
+    return (*el.at(0) + *el.at(1)).M();
+  } else if (nmu == 2) {
+    return (*mu.at(0) + *mu.at(1)).M();
+  } else {
+    throw std::logic_error(std::to_string(nel) + " electrons, " +
+			   std::to_string(nmu) + " muons in get_mll");
   }
-  
+
 }
 
 
-void set_bit(std::vector<SelectedJet*>& jets, unsigned bit) { 
-  for (std::vector<SelectedJet*>::iterator itr = jets.begin(); 
-       itr != jets.end(); itr++) { 
-    (*itr)->set_bit(bit); 
+void set_bit(std::vector<SelectedJet*>& jets, unsigned bit) {
+  for (std::vector<SelectedJet*>::iterator itr = jets.begin();
+       itr != jets.end(); itr++) {
+    (*itr)->set_bit(bit);
   }
 }
 
-void calibrate_jets(std::vector<SelectedJet*> jets, 
-		    const BtagCalibration* cal) { 
+void calibrate_jets(std::vector<SelectedJet*> jets,
+		    const BtagCalibration* cal) {
   for (auto jitr: jets) {
-    jitr->set_flavor_tag(cal); 
+    jitr->set_flavor_tag(cal);
   }
 }
 
