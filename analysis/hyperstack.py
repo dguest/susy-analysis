5c416f9d1851033a9fa227ed72c68cac6b45f0eb
Dan Guest <dguest@cern.ch>
2014-04-29 17:32:06 -0400

    whitespace

--- analysis/hyperstack.py
+++ analysis/hyperstack.py
@@ -5,7 +5,7 @@ import os, sys
 try:
     from scharm.stack.regions import sbottom_regions as sbr
     from scharm.stack.regions import Region
-    def named_region(name, yaml_dict): 
+    def named_region(name, yaml_dict):
         reg_dict = Region(yaml_dict).get_config_dict()
         reg_dict['name'] = name
         reg_dict['output_name'] = 'nminus.h5'
@@ -13,19 +13,19 @@ try:
         reg_dict['save_wt2'] = True
         return reg_dict
     all_cuts = [named_region(x,d) for x,d in sbr().items()]
-except ImportError: 
+except ImportError:
     all_cuts = [
-        { 
-            'name': 'sr', 
-            'output_name': 'nminus.h5', 
+        {
+            'name': 'sr',
+            'output_name': 'nminus.h5',
             'hists':'NMINUS',
-            'selection':'SIGNAL', 
-            'type': 'SIGNAL', 
-            'jet_tag_requirements': ['JFC_MEDIUM','JFC_MEDIUM'], 
-            'tagger':'JFC', 
-            'systematic':'NONE', 
-            'jet_tag_assignment':'PT_ORDERED', 
-            }, 
+            'selection':'SIGNAL',
+            'type': 'SIGNAL',
+            'jet_tag_requirements': ['JFC_MEDIUM','JFC_MEDIUM'],
+            'tagger':'JFC',
+            'systematic':'NONE',
+            'jet_tag_assignment':'PT_ORDERED',
+            },
         ]
 
 hfw.stacksusy(sys.argv[1], all_cuts, flags='v')
