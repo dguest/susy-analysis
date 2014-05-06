8366e7d92ea8a00808a85493d85f47e3a5f0d933
Dan Guest <dguest@cern.ch>
2014-05-06 19:27:37 -0400

    add double-tag veto to the met selection

--- analysis/src/MetSelection.cxx
+++ analysis/src/MetSelection.cxx
@@ -2,6 +2,7 @@
 #include "RegionConfig.hh"
 #include "EventObjects.hh"
 #include "constants_scharmcuts.hh"
+#include "constants_tagging.hh"
 #include "trigger_logic.hh"
 
 #include <stdexcept>
@@ -24,5 +25,13 @@ bool MetSelection::pass(const EventObjects& obj) const {
   using namespace reg;
   auto n_leptons = reco.n_baseline_electrons + reco.n_baseline_muons;
   bool zero_lepton_selection = n_leptons == 0 && reco.pass_met_trigger;
-  return zero_lepton_selection;
+
+  // veto events in SR
+  const size_t jets_to_check = std::min<size_t>(obj.jets.size(), 2);
+  int passing = 0;
+  for (auto jet_n = 0U; jet_n < jets_to_check; jet_n++) {
+    bool pass = obj.jets.at(jet_n).pass_tag(btag::JFC_MEDIUM);
+    if (pass) passing++;
+  }
+  return zero_lepton_selection && (passing < 2);
 }
