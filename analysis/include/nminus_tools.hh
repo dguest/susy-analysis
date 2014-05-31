368c7bf6b53eb013f8257a9ec775bc91f0eb0699
Dan Guest <dguest@cern.ch>
2014-05-31 18:29:49 -0400

    refactor most of the NMinus1Histograms stuff into nminus_tools

--- analysis/include/nminus_tools.hh
+++ analysis/include/nminus_tools.hh
@@ -1,13 +1,51 @@
 #ifndef SELECTION_FACTORY_HH
 #define SELECTION_FACTORY_HH
 
+#include "Window.hh"
+
 class ISelection;
 class RegionConfig;
 
 #include <map>
+#include <string>
 
 namespace nminus {
+  const int MIN_SIGNAL_JETS = 2;
+  // signal jet hist shows from -0.5 to (SJET_RANGE - 0.5)
+  const int SJET_RANGE = 15;
+  // this number or higher means "infinite"
+  const int SJET_INF_THRESHOLD = 100;
+
+  // all units are in MeV
+  const std::string EUNIT = "MeV";
+
+  // names for internal use (and saving)
+  const std::string MET = "met";
+  const std::string DPHI = "jetmet_dphi";
+  const std::string MCT = "mass_ct";
+  const std::string MET_EFF = "met_eff";
+  const std::string MCC = "mass_cc";
+  const std::string LLPT = "leading_lepton_pt";
+  const std::string SLPT = "second_lepton_pt";
+  const std::string MT = "mass_t";
+  const std::string MLL = "mass_ll";
+  const std::string NSJET = "n_signal_jets";
+  // used by the jet naming routines
+  const std::string FTL = "flavor_truth_label";
+
+  // naming functions
+  std::string jeta(int jn);
+  std::string jpt(int jn);
+  std::string jantib(int jn);
+  std::string jantiu(int jn);
+  std::string jftl(int jn);
+
+  // ACHTUNG: figure out a strategy to plot the SF
+  // const std::string TAGSF = "tag_sf";
+  // std::string jtagsf(int jn);
+
   ISelection* selection_factory(const RegionConfig& config);
+  std::map<std::string, Window> get_windows(const RegionConfig&);
 }
 
 #endif
--- analysis/src/NMinus1Histograms.cxx
+++ analysis/src/NMinus1Histograms.cxx
@@ -6,8 +6,6 @@
 #include "RegionConfig.hh"
 #include "EventObjects.hh"
 #include "EventScalefactors.hh"
-#include "constants_scharmcuts.hh"
-#include "constants_tagging.hh"
 #include "region_logic.hh"
 #include "nminus_tools.hh"
 
@@ -21,7 +19,6 @@
 #include <set>
 
 namespace nminus {
-  std::map<std::string, Window> get_windows(const RegionConfig&);
 }
 
 NMinus1Histograms
@@ -220,154 +217,5 @@ void NMinus1Histograms::write_to(H5::CommonFG& file) const {
 }
 
 namespace nminus {
-  void add_tagging_cuts(std::map<std::string, Window>& sel,
-			int n_tagged = 2);
-  // _______________________________________________________________________
-  // cut adding fucntions (shouldn't call each other)
-
-  std::map<std::string, Window> get_common_selection(
-    const RegionConfig& cfg) {
-
-    // basic kinematics (jets don't have to exist to pass this cut, but
-    // the events will fail the n_jet selection)
-    std::map<std::string, Window> sel = {
-      {jpt(0), {cfg.leading_jet_pt, INFINITY, Window::Missing::ACCEPT} },
-      {jpt(1), {cfg.second_jet_pt, INFINITY, Window::Missing::ACCEPT} },
-    };
-    if (cfg.met > 0.0) sel[MET] = {cfg.met, INFINITY};
-    if (cfg.mct > 0.0) sel[MCT] = {cfg.mct, INFINITY};
-
-    // SJET_RANGE goes from -0.5, so we only show a limit if the max jets
-    // are < the range (i.e. 0 to 7 is a range of 8, don't show if max is 8)
-    if (cfg.max_signal_jets < SJET_RANGE) {
-      sel[NSJET] = {MIN_SIGNAL_JETS - 0.5, cfg.max_signal_jets + 0.5};
-    } else if (cfg.max_signal_jets < SJET_INF_THRESHOLD){
-      throw std::logic_error(
-	"specified a cut above " + std::to_string(SJET_RANGE) + ", but below"
-	" the 'infinite' value of " + std::to_string(SJET_INF_THRESHOLD));
-    } else {
-      sel[NSJET] = {MIN_SIGNAL_JETS - 0.5, INFINITY};
-    }
-
-    // add tagging cuts
-    if (cfg.tagger == btag::Tagger::JFC_LEADING_JET) {
-      add_tagging_cuts(sel, 1);
-    } else if (cfg.tagger == btag::Tagger::JFC ||
-	       cfg.tagger == btag::Tagger::JFC_NO_SF) {
-      add_tagging_cuts(sel, 2);
-    } else if (cfg.tagger != btag::Tagger::NONE) {
-      throw std::invalid_argument("tagger isn't going to work");
-    }
-    return sel;
-  }
-  void add_tagging_cuts(std::map<std::string, Window>& sel,
-			int n_tagged) {
-    // accept events where these jets are missing
-    // (they will be rejected by the njet cut anyway)
-    auto miss = Window::Missing::ACCEPT;
-    // add tagging cuts (use of `insert` prevents overwrite)
-    for (auto jn = 0; jn < n_tagged; jn++) {
-      const auto& antib = btag::JFC_MEDIUM_ANTI_B_CUT;
-      const auto& antiu = btag::JFC_MEDIUM_ANTI_U_CUT;
-      sel.insert({jeta(jn), {-btag::TAG_ETA, btag::TAG_ETA, miss}});
-      sel.insert({jantib(jn), {antib, INFINITY, miss} });
-      sel.insert({jantiu(jn), {antiu, INFINITY, miss} });
-    }
-  }
-  void add_1l_cuts(std::map<std::string, Window>& sel) {
-    using namespace cr1l;
-    sel.insert(
-      {
-	{MT, {M_T_MIN, M_T_MAX} },
-	  });
-  }
-  void add_sf_cuts(std::map<std::string, Window>& sel) {
-    using namespace crsf;
-    sel.insert(
-      {
-	{LLPT, {LEPTON_PT_MIN, INFINITY} },
-	{MLL, {M_LL_MIN, M_LL_MAX} },
-	  });
-  }
-  void add_df_cuts(std::map<std::string, Window>& sel) {
-    using namespace crdf;
-    sel.insert(
-      {
-	{MLL, {M_LL_MIN, INFINITY} },
-	  });
-  }
-  void add_sr_cuts(std::map<std::string, Window>& sel) {
-    sel.insert( {
-	{MET_EFF, {MET_EFF_MIN, INFINITY} },
-	{MCC, {M_CC_MIN, INFINITY} },
-	{DPHI, {MIN_DPHI_JET_MET, INFINITY} },
-	  });
-  }
-  void add_presel_cuts(std::map<std::string, Window>& sel) {
-    // this case has phantom cuts to indicate where SR cuts _would_ be
-    const auto PHANTOM = Window::Missing::PHANTOM;
-    sel.insert({
-	{DPHI, {MIN_DPHI_JET_MET, INFINITY} },
-	{MCT, {SR_MCT_MIN, INFINITY, PHANTOM} },
-	{MET_EFF, {MET_EFF_MIN, INFINITY, PHANTOM} },
-	{MCC, {M_CC_MIN, INFINITY, PHANTOM} },
-	{DPHI, {MIN_DPHI_JET_MET, INFINITY, PHANTOM} },
-	  });
-  }
-
-  // ______________________________________________________________________
-  // top level selection builder (calls the above functions)
-
-  std::map<std::string, Window> get_windows(const RegionConfig& cfg)
-  {
-    auto sel = get_common_selection(cfg);
-    switch (cfg.selection) {
-    case reg::Selection::SIGNAL: {
-      add_sr_cuts(sel);
-      return sel;
-    }
-      // control / validation regions
-    case reg::Selection::CR_1E:	// fallthrough
-    case reg::Selection::CR_1M:	// fallthrough
-    case reg::Selection::CR_W: {
-      add_1l_cuts(sel);
-      return sel;
-    }
-    case reg::Selection::CR_Z_1L: // fallthrough
-    case reg::Selection::CR_Z_2L: // fallthrough
-    case reg::Selection::CR_Z: {
-      add_sf_cuts(sel);
-      return sel;
-    }
-    case reg::Selection::CR_T: {
-      add_df_cuts(sel);
-      return sel;
-    }
-    case reg::Selection::VR_MET: // fallthrough
-    case reg::Selection::QUALITY_EVENT: {
-      add_presel_cuts(sel);
-      return sel;
-    }
-
-    default: throw std::invalid_argument("unknown selection in " __FILE__);
-    }
-  }
-
-  std::string jeta(int jn) {
-    return "j" + std::to_string(jn) + "_eta";
-  }
-  std::string jpt(int jn) {
-    return "j" + std::to_string(jn) + "_pt";
-  }
-  std::string jantib(int jn) {
-    return "j" + std::to_string(jn) + "_antib";
-  }
-  std::string jantiu(int jn) {
-    return "j" + std::to_string(jn) + "_antiu";
-  }
-  std::string jftl(int jn) {
-    return "j" + std::to_string(jn) + "_" + FTL;
-  }
-
 
 }
