488fec5fe40b17ee9475e790d371a54b739a9bca
Dan Guest <dguest@cern.ch>
2014-07-10 17:09:01 -0400

    bugfixes

--- scripts/susy-check-yields.py
+++ scripts/susy-check-yields.py
@@ -14,16 +14,16 @@ from scharm.aggregate import histread
 def run():
     # _dump_counts({(43, 'smag'): dict(w=20, n=8)})
     args = get_args()
-    h5_files = _get_files(args.h5_files)
+    h5_files = []
+    h5_files += _get_files(args.h5_files)
     quiet = not args.verbose
 
     counts = {}
 
     normalizer = Normalizer(args.meta, h5_files, quiet=quiet)
     for dsid, hfile, norm in normalizer.byid():
-        all_ids.append(dsid)
         for region in args.regions:
-            weighted_counts = histread.get_counts(hfile[region]['met'])
+            weighted_counts = histread.get_count(hfile[region]['met'])
             counts[dsid,region] = {
                 'w': weighted_counts, 'n': weighted_counts * norm}
 
@@ -33,7 +33,7 @@ def get_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('h5_files', nargs='+')
     parser.add_argument('-m', '--meta', required=True)
-    parser.add_argument('-r', '--regions', nargs='+', default='cr_t')
+    parser.add_argument('-r', '--regions', nargs='+', default=['cr_t'])
     parser.add_argument('-v', '--verbose', action='store_true')
 
     return parser.parse_args(sys.argv[1:])
@@ -48,7 +48,12 @@ def _dump_counts(counts):
         regions.add(region)
 
     sreg = sorted(regions)
-    fmt_string = '{:10} | ' + '{:12.2f} {:12.2f} | '*len(sreg)
+    fmt_tmp = '{:>10} | ' + '{XXX} {XXX} | '*len(sreg)
+    fmt_string = fmt_tmp.replace('XXX', ':12.2f')
+    reg_args = [''] + sreg
+    print(fmt_tmp.replace('XXX} {XXX', ':^25').format(*reg_args))
+    title_args = ['dsid'] + ['weighted', 'normed']*len(sreg)
+    print(fmt_tmp.replace('XXX', ':>12').format(*title_args))
     for dsid in sorted(ids):
         fmt_list = [dsid]
         for region in sreg:
@@ -59,8 +64,8 @@ def _get_files(fandd):
     """convert a list of files and dirs to a list of files"""
     files = []
     for entry in fandd:
-        if isdir(fandd):
-            for root, dirs, files in walk(fandd):
+        if isdir(entry):
+            for root, dirs, files in walk(entry):
                 for hf in files:
                     if hf.endswith('.h5'):
                         files.append(hf)
