5d8e88ef704fcbe6f3e1bc910a42cf1ef2be7fd5
Dan Guest <dguest@cern.ch>
2014-07-17 20:49:37 -0400

    added mettrig unct to systematic merger

--- scripts/susy-fit-addsyst.py
+++ scripts/susy-fit-addsyst.py
@@ -4,6 +4,10 @@
 import argparse
 import yaml
 
+_nom_key = 'nominal_yields'
+_yld_key = 'yield_systematics'
+_rel_key = 'relative_systematics'
+
 def get_config():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('fit_inputs')
@@ -14,6 +18,7 @@ def run():
     with open(args.fit_inputs, 'r+') as yml:
         yields_dict = yaml.load(yml)
         _add_qcd(yields_dict)
+        _add_mettrig(yields_dict)
         yml.truncate(0)
         yml.seek(0)
         yml.write(yaml.dump(yields_dict))
@@ -25,7 +30,17 @@ def _add_qcd(yields_dict):
         'signal_mct250': 0.05,
         }
     for sr, count in sr_qcd.items():
+        # give the qcd 100% statistical uncertainty
         yields_dict['nominal_yields'][sr]['QCD'] = [count, count]
+
+def _add_mettrig(yields_dict, error=0.01):
+    """adds a 1% uncertainty to all the signal regions"""
+    nom = yields_dict[_nom_key]
+    srs = [x for x in nom if x.startswith('signal_')]
+    relsyst = yields_dict.setdefault(_rel_key, {})
+    mettrig = relsyst.setdefault('mettrig', {})
+    for sr in srs:
+        mettrig[sr] = [1 - error, 1 + error]
 
 if __name__ == '__main__':
     run()
