3274ada51352bee6dcc85e5b3e7e4fe13e715d21
Dan Guest <dguest@cern.ch>
2014-07-28 20:21:01 -0400

    add isr systematics

--- scripts/susy-fit-addsyst.py
+++ scripts/susy-fit-addsyst.py
@@ -3,10 +3,12 @@
 
 import argparse
 import yaml
+import bisect
 
 _nom_key = 'nominal_yields'
 _yld_key = 'yield_systematics'
 _rel_key = 'relative_systematics'
+_sig_key = 'scharm-'
 
 _misc_syst_help = 'update with an arbitrary dictionary (will overwrite)'
 
@@ -24,6 +26,7 @@ def run():
         yields_dict = yaml.load(yml)
         _add_qcd(yields_dict)
         _add_mettrig(yields_dict, error=args.met_trig_error)
+        _add_signal_isr(yields_dict)
         _add_misc(yields_dict, args.misc_syst)
         yml.truncate(0)
         yml.seek(0)
@@ -63,6 +66,37 @@ def _add_misc(yields_dict, misc):
     with open(misc) as yml:
         new_dict = yaml.load(yml)
     yields_dict[_rel_key].update(new_dict[_rel_key])
+
+def _add_signal_isr(yields_dict):
+    """Add an isr systematic (taken from sbottom) to the signal points"""
+
+    # pulled from sbottom
+    dm_vs_isr_syst = [
+        (0, 0.287),
+        (10, 0.076),
+        (30, 0.079),
+        (50, 0.112),
+        ]
+
+    dm_vals, ret_vals = zip(*dm_vs_isr_syst)
+    def get_isr(sig_name):
+        prefix, msusy, mlsp = sig_name.split('-')
+        dm = int(msusy) - int(mlsp)
+        idx = bisect.bisect(dm_vals, dm) - 1
+        return ret_vals[idx]
+
+    nomv = yields_dict[_nom_key].values()
+    all_sigp = {x for y in nomv for x in y if x.startswith(_sig_key)}
+    all_sr = {x for x in yields_dict[_nom_key] if x.startswith('signal_')}
+
+    isr_syst = {}
+    for sigp in all_sigp:
+        isr_val = get_isr(sigp)
+        variation = [1.0 - isr_val, 1.0 + isr_val]
+        for sr in all_sr:
+            isr_syst.setdefault(sr,{})[sigp] = variation
+
+    yields_dict[_rel_key]['signal_isr'] = isr_syst
 
 if __name__ == '__main__':
     run()
