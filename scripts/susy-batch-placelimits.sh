b18f872b5f56759c52d150662318eae37232042a
Dan Guest <dguest@cern.ch>
2014-09-09 20:58:30 -0400

    script to copy tables into paper

--- scripts/susy-batch-placelimits.sh
+++ scripts/susy-batch-placelimits.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 
 # sort tables / figures
-for 
+figs=figures/limit_tree
+for fig in $(find $1 -type f -name '*.pdf')
+do
+    tail=${fig#*$1}
+    dest=$figs/$tail
+    mkdir -p $(dirname $dest)
+    cp $fig $dest
+done
+
+tables=tables/generated
+for table in $(find $1 -type f -name '*.tex')
+do
+    tail=${table#*$1}
+    dest=$tables/$tail
+    mkdir -p $(dirname $dest)
+    if [[ $table == */systable.tex || $table == */yieldtable.tex ]]
+	then
+	susy-fit-translate-defs.py $table >| $dest
+    else
+	cp $table $dest
+    fi
+done
