5d47d776f18f9a4292ea91d3a753c6865fe12068
Dan Guest <dguest@cern.ch>
2014-10-31 17:16:48 -0400

    add some more comments

--- analysis/src/TTBarReweighting.cxx
+++ analysis/src/TTBarReweighting.cxx
@@ -1,8 +1,15 @@
 #include "TTBarReweighting.hh"
+#include <stdexcept>
 
 float get_ttbar_reweighting(float pt) {
   // copied from an email from Will, who probably got it from Xiaoxiao
-  // or Anna
+  // or Anna here:
+  // https://svnweb.cern.ch/trac/atlasinst/browser/Institutes/Yale/trunk/1LStopBoosted/Root/SelectionTools.cxx#L353
+  // They claim they got this from ATLAS-CONF-2013-099 (ttbar diff xsec)
+  if (pt < 0.0) {
+    throw std::domain_error(
+      "tried to calculate ttbar pt reweighting for event with negative pt");
+  }
   if (pt < 40) {
     return ((1/1.01185 + 1/0.994193)/2);
   } else if (pt < 170) {
